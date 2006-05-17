87ac5674ec5bfc53294c21b0c74292e625795251
Greg Bolsinga <greg@bolsinga.com>
2006-05-17 16:26:10 +0000

    use an itunes backup directory.
    git-svn-id: file:///svn/repository/bin_utils/trunk@43 a1e501d5-4615-0410-8dc9-a46690435e56

--- database_building_scripts/backup_itunes.sh
+++ database_building_scripts/backup_itunes.sh
@@ -1,3 +1,4 @@
+
 #!/bin/sh
 MDATE=`date "+%Y-%m-%d"`
-cp $HOME/Music/iTunes/iTunes\ Music\ Library.xml $HOME/Documents/sources/data/iTunes-$MDATE.xml
+cp $HOME/Music/iTunes/iTunes\ Music\ Library.xml $HOME/Documents/sources/data/itunes_backups/iTunes-$MDATE.xml
