67bc1aee9230e24e93a41853cf3404c3fb93536d
Greg Bolsinga <greg@bolsinga.com>
2006-05-24 16:28:39 +0000

    create and use generic quit_app.sh script to quit iPhoto.
    git-svn-id: file:///svn/repository/bin_utils/trunk@46 a1e501d5-4615-0410-8dc9-a46690435e56

---
+++ database_building_scripts/quit_app.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Quit app if it is running
+appname=$1
+app=`ps -wwwaux | grep $appname\.app | grep -v grep`
+if [ "$app" ]
+then
+/usr/bin/osascript <<EOF
+tell application "$appname"
+quit
+end tell
+EOF
+sleep 10
+fi
