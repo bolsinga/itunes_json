21a4dfc823fcbeb40185c8a64072d95d6a65dd4c
Greg Bolsinga <greg@bolsinga.com>
2006-06-02 22:05:11 +0000

    first added
    git-svn-id: file:///svn/repository/bin_utils/trunk@49 a1e501d5-4615-0410-8dc9-a46690435e56

---
+++ database_building_scripts/convert_cvs2svn.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+CVS_PATH=$1
+PROJECT=$2
+
+if [ -z $CVS_PATH ]
+then
+ echo Need to supply a cvs repository path.
+ exit 1
+fi
+if [ -z $PROJECT ]
+then
+ echo Need to supply a project name for the svn repository.
+ exit 1
+fi
+
+CVS2SVN=/Data/Applications/development/cvs2svn-1.3.0/cvs2svn
+
+SVN_REPOS=/svn/repository
+CVS_REPOS=/cvs/repository
+
+TMP_DIR=`pwd`
+TMP_DIR=$TMP_DIR/$PROJECT
+
+TMP_CVS=$TMP_DIR/tmpcvs
+
+# Create a 'fake' cvs repository that only has the project we are interested in
+echo "Copying $CVS_REPOS/$CVS_PATH into $TMP_CVS..."
+mkdir -p $TMP_CVS/CVSROOT
+cp -r $CVS_REPOS/$CVS_PATH/ $TMP_CVS/
+
+cd $TMP_DIR
+
+# Use cvs2svn to make a dump file of the 'fake' cvs repository.
+$CVS2SVN --dump-only $TMP_CVS
+
+IMPORT_SH=$TMP_DIR/import_$PROJECT.sh
+echo "Creating $IMPORT_SH to be run after editing the dump filter in $TMP_DIR."
+cat > $IMPORT_SH <<EOF
+#!/bin/sh
+# Set up a directory in svn for this project and import it
+svn mkdir file://$SVN_REPOS/$PROJECT -m "Add $PROJECT project directory."
+echo "Importing $PROJECT from $TMP_CVS into $SVN_REPOST/$PROJECT"
+svnadmin --parent-dir $PROJECT load $SVN_REPOS < $TMP_DIR/cvs2svn-dump
+EOF
+chmod +x $IMPORT_SH
