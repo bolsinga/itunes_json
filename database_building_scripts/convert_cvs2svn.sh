5d02edd698c9c8a4d6f995694954c2495ee489e5
Greg Bolsinga <greg@bolsinga.com>
2006-06-02 23:50:14 +0000

    allow the cvs2svn-dump file name to be passed in as a parameter, defaulting to the default generated file.
    git-svn-id: file:///svn/repository/bin_utils/trunk@51 a1e501d5-4615-0410-8dc9-a46690435e56

--- database_building_scripts/convert_cvs2svn.sh
+++ database_building_scripts/convert_cvs2svn.sh
@@ -38,9 +38,18 @@ IMPORT_SH=$TMP_DIR/import_$PROJECT.sh
 echo "Creating $IMPORT_SH to be run after editing the dump filter in $TMP_DIR."
 cat > $IMPORT_SH <<EOF
 #!/bin/sh
+
+DUMP_FILE=\$1
+
+if [ -z \$DUMP_FILE ]
+then
+    echo "svn dump file not supplied; assuming $TMP_DIR/cvs2svn-dump."
+    DUMP_FILE=$TMP_DIR/cvs2svn-dump
+fi
+
 # Set up a directory in svn for this project and import it
 svn mkdir file://$SVN_REPOS/$PROJECT -m "Add $PROJECT project directory."
 echo "Importing $PROJECT from $TMP_CVS into $SVN_REPOST/$PROJECT"
-svnadmin --parent-dir $PROJECT load $SVN_REPOS < $TMP_DIR/cvs2svn-dump
+svnadmin --parent-dir $PROJECT load $SVN_REPOS < \$DUMP_FILE
 EOF
 chmod +x $IMPORT_SH
