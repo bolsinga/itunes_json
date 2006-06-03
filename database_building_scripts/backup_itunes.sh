fb303c76b8860f455d6ef4101b12344097f8cc59
Greg Bolsinga <greg@bolsinga.com>
2006-06-03 03:34:49 +0000

    move the location of the itunes_backups.
    git-svn-id: file:///svn/repository/bin_utils/trunk@1692 a1e501d5-4615-0410-8dc9-a46690435e56

--- database_building_scripts/backup_itunes.sh
+++ database_building_scripts/backup_itunes.sh
@@ -1,4 +1,3 @@
-
 #!/bin/sh
 MDATE=`date "+%Y-%m-%d"`
-cp $HOME/Music/iTunes/iTunes\ Music\ Library.xml $HOME/Documents/sources/data/itunes_backups/iTunes-$MDATE.xml
+cp $HOME/Music/iTunes/iTunes\ Music\ Library.xml $HOME/Documents/sources/itunes_backups/iTunes-$MDATE.xml
