82e0b9b4db80e384adb654ef1f5e60794c079bfd
Greg Bolsinga <greg@bolsinga.com>
2007-12-09 21:02:51 +0000

    get the proper directory
    git-svn-id: file:///svn/repository/bin_utils/trunk@2659 a1e501d5-4615-0410-8dc9-a46690435e56

--- database_building_scripts/backup_itunes.sh
+++ database_building_scripts/backup_itunes.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 MDATE=`date "+%Y-%m-%d"`
-cp $HOME/Music/iTunes/iTunes\ Music\ Library.xml $HOME/Documents/sources/itunes_backups/iTunes-$MDATE.xml
+cp $HOME/Music/iTunes/iTunes\ Music\ Library.xml $HOME/Documents/code/itunes_backups/iTunes-$MDATE.xml
