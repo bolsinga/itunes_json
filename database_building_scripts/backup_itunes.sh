425a5d28ae9a5e56cf7e85bad0de242d04077fa3
Greg Bolsinga <bolsinga@mac.com>
2020-11-01 10:45:19 -0800

    use itunes_json when backing up itunes data
    when building the archive of itunes_json from Xcode, extract the product into ~/Applications, and remove the date/time from the default output directory in the UI.

--- database_building_scripts/backup_itunes.sh
+++ database_building_scripts/backup_itunes.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 MDATE=`date "+%Y-%m-%d"`
-cp $HOME/Music/iTunes/iTunes\ Music\ Library.xml $HOME/Documents/code/itunes_backups/iTunes-$MDATE.xml
+$HOME/Applications/itunes_json/Products/usr/local/bin/itunes_json > $HOME/Documents/code/itunes_backups/iTunes-$MDATE.json
