5a24cb4055404d61837414092e60605894eefb54
Greg Bolsinga <bolsinga@mac.com>
2020-11-01 08:28:52 -0800

    Use simple stdErr instead of Logging library (#7)
    This is basically because I do not want to figure out how to package the logging framework for use by this cmd line tool. Without it the build archive does not run since it can't find the framework. This simplifies things nicely.

--- Package.swift
+++ Package.swift
@@ -8,14 +8,11 @@ let package = Package(
     platforms: [
         .macOS(.v10_14),
     ],
-    dependencies: [
-        .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
-    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "itunes_json",
-            dependencies: ["Logging"]),
+            dependencies: []),
     ]
 )
