f76f4e107d2528fa1384c1337b95bcbaa58ea10a
Greg Bolsinga <bolsinga@mac.com>
2020-11-02 14:43:19 -0800

    target macOS 10.15

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "itunes_json",
     platforms: [
-        .macOS(.v10_14),
+        .macOS(.v10_15),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
