92d7ee0cbb2b62258d363cacc230882d2563309a
Greg Bolsinga <bolsinga@mac.com>
2020-11-25 20:07:07 -0800

    clear out the destination directory before building site

--- database_building_scripts/build_site.sh
+++ database_building_scripts/build_site.sh
@@ -14,6 +14,8 @@ if [ -z "$SITE_DIR" ] ; then
     exit 1
 fi
 
+rm -rf $SITE_DIR/*
+
 rm /tmp/itunes-*.json
 
 MDATE=`date "+%Y-%m-%d-%H:%M:%S"`
@@ -21,6 +23,8 @@ ITUNES_JSON_FILE=/tmp/itunes-$MDATE.json
 
 $HOME/Applications/itunes_json/Products/usr/local/bin/itunes_json > $ITUNES_JSON_FILE
 if_failure "Cannot create itunes json file: $ITUNES_JSON_FILE"
+
+cp -R $HOME/Documents/code/git/web_data/html/ $SITE_DIR
 
 $HOME/Applications/web_generator/site $HOME/Documents/code/git/web_data $SITE_DIR site $ITUNES_JSON_FILE
 if_failure "Cannot create site locally"
