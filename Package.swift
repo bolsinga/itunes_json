577815e51c77d011a82a20b3c89f8cc32e42e1c0
Greg Bolsinga <bolsinga@mac.com>
2021-10-30 14:47:48 -0700

    Update to macos 12 and Swift 5.5

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.4
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "itunes_json",
     platforms: [
-        .macOS(.v11),
+        .macOS(.v12),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
