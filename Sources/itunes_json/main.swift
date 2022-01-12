b1ef322f72871cfdd51f987e2ee392b5c6169f42
Greg Bolsinga <bolsinga@mac.com>
2022-01-11 21:14:40 -0800

    Use Foundation String.write for simplicity

--- Sources/itunes_json/main.swift
+++ Sources/itunes_json/main.swift
@@ -1,36 +1,5 @@
 import Foundation
-import func Darwin.fputs
 import var Darwin.stderr
-
-struct FileHandlerOutputStream: TextOutputStream {
-    private let fileHandle: FileHandle
-    let encoding: String.Encoding
-
-    init(_ fileHandle: FileHandle, encoding: String.Encoding = .utf8) {
-        self.fileHandle = fileHandle
-        self.encoding = encoding
-    }
-
-    mutating func write(_ string: String) {
-        if let data = string.data(using: encoding) {
-            fileHandle.write(data)
-        }
-    }
-}
-
-var fileHandle : FileHandle?
-if (CommandLine.arguments.count > 1) {
-    let destinationDirectoryPath = CommandLine.arguments[1]
-    var destinationURL = URL(fileURLWithPath: destinationDirectoryPath, isDirectory: true)
-
-    let dateFormatter = DateFormatter();
-    dateFormatter.dateFormat = "yyyy-MM-dd"
-    let dateString = dateFormatter.string(from: Date())
-
-    destinationURL.appendPathComponent("iTunes-\(dateString).json")
-    FileManager.default.createFile(atPath: destinationURL.path, contents: nil, attributes: nil)
-    fileHandle = try FileHandle(forWritingTo: destinationURL)
-}
 
 struct StderrOutputStream: TextOutputStream {
     mutating func write(_ string: String) {
@@ -63,9 +32,18 @@ guard let jsonString = String(data: jsonData, encoding: .utf8) else {
     exit(1)
 }
 
-if let outputFileHandle = fileHandle {
-    var standardOutput = FileHandlerOutputStream(outputFileHandle)
-    print("\(jsonString)", to: &standardOutput)
+if (CommandLine.arguments.count > 1) {
+    let destinationDirectoryPath = CommandLine.arguments[1]
+    var destinationURL = URL(fileURLWithPath: destinationDirectoryPath, isDirectory: true)
+
+    let dateFormatter = DateFormatter();
+    dateFormatter.dateFormat = "yyyy-MM-dd"
+    let dateString = dateFormatter.string(from: Date())
+
+    destinationURL.appendPathComponent("iTunes-\(dateString).json")
+    FileManager.default.createFile(atPath: destinationURL.path, contents: nil, attributes: nil)
+
+    try jsonString.write(toFile: destinationURL.path, atomically: true, encoding: .utf8)
 } else {
     print("\(jsonString)")
 }
