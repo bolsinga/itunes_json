f80d6702ccf0728f615355983eaf40af0b7ffa7a
Greg Bolsinga <bolsinga@mac.com>
2022-04-17 10:44:56 -0700

    Lint files with swift-format 5.6

--- Package.swift
+++ Package.swift
@@ -4,18 +4,18 @@
 import PackageDescription
 
 let package = Package(
-    name: "itunes_json",
-    platforms: [
-        .macOS(.v12),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.1.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .executableTarget(
-            name: "itunes_json",
-            dependencies: [.product(name: "ArgumentParser", package: "swift-argument-parser"),]),
-    ]
+  name: "itunes_json",
+  platforms: [
+    .macOS(.v12)
+  ],
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-argument-parser", from: "1.1.0")
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+    // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+    .executableTarget(
+      name: "itunes_json",
+      dependencies: [.product(name: "ArgumentParser", package: "swift-argument-parser")])
+  ]
 )
