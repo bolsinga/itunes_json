d6922b460947ac954fdc2eac0eb0b28a797a8262
Greg Bolsinga <bolsinga@mac.com>
2022-10-16 18:20:37 -0700

    refactor exporting code into Track extensions (#33)

--- Sources/itunes_json/Program.swift
+++ Sources/itunes_json/Program.swift
@@ -11,40 +11,10 @@ struct Program: ParsableCommand {
   var directoryPath: String = ""
 
   func run() throws {
-    guard let tracks = try? Track.gatherAllTracks() else {
-      throw ValidationError("Cannot get tracks from iTunes")
-    }
-
-    guard tracks.count > 0 else {
-      throw ValidationError("No JSON to record")
-    }
-
-    let encoder = JSONEncoder()
-    encoder.outputFormatting = [.prettyPrinted, .sortedKeys]
-    encoder.dateEncodingStrategy = .iso8601
-
-    guard let jsonData = try? encoder.encode(tracks) else {
-      throw ValidationError("Unable to create JSON for \(tracks)")
-    }
-
     if !directoryPath.isEmpty {
-      let destinationDirectoryPath = directoryPath
-      var destinationURL = URL(fileURLWithPath: destinationDirectoryPath, isDirectory: true)
-
-      let dateFormatter = DateFormatter()
-      dateFormatter.dateFormat = "yyyy-MM-dd"
-      let dateString = dateFormatter.string(from: Date())
-
-      destinationURL.appendPathComponent("iTunes-\(dateString).json")
-      FileManager.default.createFile(atPath: destinationURL.path, contents: nil, attributes: nil)
-
-      try jsonData.write(to: destinationURL, options: .atomic)
+      try Track.export(directoryPath)
     } else {
-      guard let jsonString = String(data: jsonData, encoding: .utf8) else {
-        throw ValidationError("Unable to create JSON string for \(tracks)")
-      }
-
-      print("\(jsonString)")
+      print("\(try Track.jsonString())")
     }
   }
 }
