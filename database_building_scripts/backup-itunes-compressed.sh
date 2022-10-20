631ffc4bce06b2c74c90ccb7e0c2e7c089257caa
Greg Bolsinga <bolsinga@mac.com>
2022-10-19 19:09:06 -0700

    add shell script to back up itunes json into a compressed gz file. reduces filesize by 90%

---
+++ database_building_scripts/backup-itunes-compressed.sh
@@ -0,0 +1,19 @@
+#/bin/sh
+
+if_failure ()
+{
+  if [ "$?" -ne 0 ] ; then
+    echo "$0: Failure Error: $1" 1>&2
+    exit 1
+  fi
+}
+
+DST_DIR="$1"
+if [ -z "$DST_DIR" ] ; then
+    echo "No destination directory"  1>&2
+    exit 1
+fi
+
+MDATE=`date "+%Y-%m-%d"`
+
+/Users/bolsinga/Applications/itunes_json/Products/usr/local/bin/itunes_json | gzip -c > $DST_DIR/iTunes-$MDATE.json.gz
