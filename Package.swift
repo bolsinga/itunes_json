8a84be403efa1a722f189cf1e7543a718f99727c
Greg Bolsinga <bolsinga@mac.com>
2023-02-22 13:12:30 -0800

    target macos13 (#35)

--- Package.swift
+++ Package.swift
@@ -5,14 +5,12 @@ import PackageDescription
 let package = Package(
   name: "itunes_json",
   platforms: [
-    .macOS(.v12)
+    .macOS(.v13)
   ],
   dependencies: [
     .package(url: "https://github.com/apple/swift-argument-parser", from: "1.1.4")
   ],
   targets: [
-    // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-    // Targets can depend on other targets in this package, and on products in packages which this package depends on.
     .executableTarget(
       name: "itunes_json",
       dependencies: [.product(name: "ArgumentParser", package: "swift-argument-parser")])
