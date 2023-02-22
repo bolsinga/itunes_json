b867d1aa1c2d92fc3dc160132db4898d7f9aaf7d
Greg Bolsinga <bolsinga@mac.com>
2023-02-21 16:30:42 -0800

    add json site build script

---
+++ database_building_scripts/build_json_site.sh
@@ -0,0 +1,28 @@
+#!/bin/sh -e
+
+if_failure ()
+{
+  if [ "$?" -ne 0 ] ; then
+    echo "$0: Failure Error: $1" 1>&2
+    exit 1
+  fi
+}
+
+SITE_DIR="$1"
+if [ -z "$SITE_DIR" ] ; then
+    echo "No site_dir"  1>&2
+    exit 1
+fi
+
+JSON_DIR="$2"
+if [ -z "$JSON_DIR" ] ; then
+  echo "No json_dir" 1>&2
+  exit 1
+fi
+
+rm -rf $SITE_DIR/*
+
+cp -a $HOME/Documents/code/git/web_data/html/ $SITE_DIR
+
+$HOME/Applications/web_generator/site json-site $SITE_DIR $HOME/Documents/code/git/web_data/settings.properties $JSON_DIR
+if_failure "Cannot create site locally"
