253f85405c54045bbbba156a2627f47bc7ed3e21
Greg Bolsinga <bolsinga@mac.com>
2023-02-24 13:55:50 -0800

    convert argument to URL while parsing

--- Sources/tool/Program.swift
+++ Sources/tool/Program.swift
@@ -7,13 +7,21 @@ struct Program: ParsableCommand {
 
   @Argument(
     help:
-      "The path at which to create an iTunes JSON file. Writes JSON to standard output if not provided."
+      "The path at which to create an iTunes JSON file. Writes JSON to standard output if not provided.",
+    transform: ({
+      let url = URL(filePath: $0, directoryHint: .isDirectory)
+      let manager = FileManager.default
+      if !manager.fileExists(atPath: url.relativePath) {
+        try manager.createDirectory(at: url, withIntermediateDirectories: true)
+      }
+      return url
+    })
   )
-  var directoryPath: String = ""
+  var outputDirectoryURL: URL? = nil
 
   func run() throws {
-    if !directoryPath.isEmpty {
-      try Track.export(directoryPath)
+    if let outputDirectoryURL {
+      try Track.export(outputDirectoryURL)
     } else {
       print("\(try Track.jsonString())")
     }
