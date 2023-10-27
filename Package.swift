a5b32882ec13242ac77fc2948abcdf2d1a816433
Greg Bolsinga <bolsinga@mac.com>
2023-10-26 18:41:52 -0700

    target macos 14

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
   name: "itunes_json",
   platforms: [
-    .macOS(.v13)
+    .macOS(.v14)
   ],
   products: [
     .library(name: "iTunes", targets: ["iTunes"]),
