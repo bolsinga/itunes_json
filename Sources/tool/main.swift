e6bc514840d46978bfdd3aeaa0b23d079e1f4824
Greg Bolsinga <bolsinga@mac.com>
2023-12-11 09:15:48 -0800

    Manually parse arguments to prepare for stdin (#77)
    - main.swift is a special file that just "runs code top to bottom".
    -- Use this in a future diff to read from stdin
    - Program struct is no longer annotated with @main since main.swift will now do the work.

---
+++ Sources/tool/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//
+//
+//  Created by Greg Bolsinga on 12/11/23.
+//
+
+import Foundation
+
+let command = Program.parseOrExit()
+do {
+    try await command.run()
+} catch {
+  Program.exit(withError: error)
+}
