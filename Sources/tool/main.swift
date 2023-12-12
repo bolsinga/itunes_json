e4c232889049c7cd8b105909841a02a0b7aa6938
Greg Bolsinga <bolsinga@mac.com>
2023-12-11 18:16:04 -0800

    allow tool to read from stdin (#81)
    - got this idea from: https://rderik.com/blog/understanding-the-swift-argument-parser-and-working-with-stdin/

--- Sources/tool/main.swift
+++ Sources/tool/main.swift
@@ -7,7 +7,34 @@
 
 import Foundation
 
-let command = Program.parseOrExit()
+func readSTDIN() -> String? {
+  var input: String = ""
+
+  while let line = readLine() {
+    if input.isEmpty {
+      input = line
+    } else {
+      input += "\n" + line
+    }
+  }
+
+  return input
+}
+
+var text: String?
+
+if CommandLine.arguments.last == "-" {
+  CommandLine.arguments.removeLast()
+
+  text = readSTDIN()
+}
+
+var arguments = Array(CommandLine.arguments.dropFirst())
+if let text = text {
+  arguments.insert(text, at: 0)
+}
+
+let command = Program.parseOrExit(arguments)
 do {
   try await command.run()
 } catch {
