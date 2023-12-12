bolsinga/itunes_json

commit 7767f304066310372460926d2cf73ade0f8c9a8f

separate into library and tool (#37)
separate into libary and tool

Package.swift
@@ -7,12 +7,19 @@ let package = Package(
   platforms: [
     .macOS(.v13)
   ],
+  products: [
+    .library(name: "iTunes", targets: ["iTunes"]),
+    .executable(name: "tool", targets: ["tool"]),
+  ],
   dependencies: [
     .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.2")
   ],
   targets: [
+    .target(name: "iTunes"),
     .executableTarget(
-      name: "itunes_json",
-      dependencies: [.product(name: "ArgumentParser", package: "swift-argument-parser")])
+      name: "tool",
+      dependencies: [
+        .byName(name: "iTunes"), .product(name: "ArgumentParser", package: "swift-argument-parser"),
+      ]),
   ]
 )

Sources/iTunes/Track.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct Track: Codable {
+public struct Track: Codable {
   var album: String? = nil  // "Album"
   var albumArtist: String? = nil  // "Album Artist"
   var albumRating: Int? = nil  // "Album Rating"

Sources/tool/Program.swift
@@ -1,5 +1,6 @@
 import ArgumentParser
 import Foundation
+import iTunes
 
 @main
 struct Program: ParsableCommand {

commit 7151bd2ba3b57b8a805a8a87002016f23b9e91bd

fix executable name
- broke with #37

Package.swift
@@ -9,7 +9,7 @@ let package = Package(
   ],
   products: [
     .library(name: "iTunes", targets: ["iTunes"]),
-    .executable(name: "tool", targets: ["tool"]),
+    .executable(name: "itunes_json", targets: ["tool"]),
   ],
   dependencies: [
     .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.3")