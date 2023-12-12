2b1098234c0653367f12d49f32f7eb3a06f6040b
Greg Bolsinga <bolsinga@mac.com>
2023-12-11 18:11:44 -0800

    format

--- Sources/iTunes/Track+String.swift
+++ Sources/iTunes/Track+String.swift
@@ -13,7 +13,9 @@ enum JSONDecodingError: Error {
 
 extension Track {
   static public func createFromString(_ source: String?) throws -> [Track] {
-    guard let source else { preconditionFailure("Should have been caught during ParsableArguments.validate().") }
+    guard let source else {
+      preconditionFailure("Should have been caught during ParsableArguments.validate().")
+    }
     guard let data = source.data(using: .utf8) else { throw JSONDecodingError.stringEncodingError }
 
     let decoder = JSONDecoder()
