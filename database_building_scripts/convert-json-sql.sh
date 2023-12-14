4b53984c9dc24bcd2860c3df14c378548129507a
Greg Bolsinga <bolsinga@mac.com>
2023-12-14 11:10:51 -0800

    add script to convert itunes json into sql

---
+++ database_building_scripts/convert-json-sql.sh
@@ -0,0 +1,20 @@
+#/bin/sh
+
+BKUP_DIR="$1"
+if [ -z "$BKUP_DIR" ] ; then
+    echo "No backup directory"  1>&2
+    exit 1
+fi
+
+JSON_TOOL=~/Applications/itunes_json/Products/usr/local/bin/itunes_json
+
+SUFFIX=".json.gz"
+
+SQL_DIR=$BKUP_DIR/../sql/
+mkdir -p $SQL_DIR
+
+for F in $(find $BKUP_DIR/ -type f | sort | grep "iTunes-\d\d\d\d-\d\d-\d\d$SUFFIX") ; do
+  NAME=`basename -s"$SUFFIX" $F`
+  echo "Processing $NAME"
+  gzip -cd $F | $JSON_TOOL --json-string --sql-code - | gzip -c > $SQL_DIR/$NAME.sql.gz
+done
