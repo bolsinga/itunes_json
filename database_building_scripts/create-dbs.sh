1232961d9b39b0381fc8c2800f1a4a4df9fb2f34
Greg Bolsinga <bolsinga@mac.com>
2023-12-15 08:54:57 -0800

    Add script to create sqlite dbs from sql files

---
+++ database_building_scripts/create-dbs.sh
@@ -0,0 +1,20 @@
+#/bin/sh
+
+SQL_DIR="$1"
+if [ -z "$SQL_DIR" ] ; then
+    echo "No SQL directory"  1>&2
+    exit 1
+fi
+
+JSON_TOOL=~/Applications/itunes_json/Products/usr/local/bin/itunes_json
+
+SUFFIX=".sql.gz"
+
+DB_DIR=$SQL_DIR/../dbs/
+mkdir -p $DB_DIR
+
+for F in $(find $SQL_DIR/ -type f | sort | grep "iTunes-\d\d\d\d-\d\d-\d\d$SUFFIX") ; do
+  NAME=`basename -s"$SUFFIX" $F`
+  echo "Processing $NAME"
+  echo "gzip -cd $F | sqlite3 > $DB_DIR/$NAME.db"
+done
