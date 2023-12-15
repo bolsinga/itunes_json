130130b94ecaae514ee00a2f7adb2f0253b4befe
Greg Bolsinga <bolsinga@mac.com>
2023-12-14 19:01:43 -0800

    Add repair json script

---
+++ database_building_scripts/repair-json.sh
@@ -0,0 +1,20 @@
+#/bin/sh
+
+BKUP_DIR="$1"
+if [ -z "$BKUP_DIR" ] ; then
+    echo "No backup directory"  1>&2
+    exit 1
+fi
+
+JSON_TOOL=~/Applications/itunes_json/Products/usr/local/bin/itunes_json
+
+SUFFIX=".json.gz"
+
+REPAIR_DIR=$BKUP_DIR/../repair/
+mkdir -p $REPAIR_DIR
+
+for F in $(find $BKUP_DIR/ -type f | sort | grep "iTunes-\d\d\d\d-\d\d-\d\d$SUFFIX") ; do
+  NAME=`basename -s"$SUFFIX" $F`
+  echo "Processing $NAME"
+  gzip -cd $F | $JSON_TOOL --repair --json-string --json - | gzip -c > $REPAIR_DIR/$NAME$SUFFIX
+done
