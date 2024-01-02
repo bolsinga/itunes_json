ec28afabca65835c6fdf2529e7ef312ae8bed487
Greg Bolsinga <bolsinga@mac.com>
2024-01-02 14:36:35 -0800

    raise empty array checks up one level

--- Sources/iTunes/Destination+Tracks.swift
+++ Sources/iTunes/Destination+Tracks.swift
@@ -7,9 +7,17 @@
 
 import Foundation
 
+enum DataExportError: Error {
+  case noTracks
+}
+
 extension Destination {
   public func emit(_ tracks: [Track], outputFile: URL?) throws {
-    let data = try tracks.data(for: self)
+    guard tracks.count > 0 else {
+      throw DataExportError.noTracks
+    }
+
+    let data = try self.data(for: tracks)
 
     if let outputFile {
       try data.write(to: outputFile, options: .atomic)
