69611f526f3fd54851e6fd391428b47b00a74483
Greg Bolsinga <bolsinga@mac.com>
2024-01-02 11:06:21 -0800

    artistName is now fileprivate Track extension

--- Sources/iTunes/Track+RowArtist.swift
+++ Sources/iTunes/Track+RowArtist.swift
@@ -6,8 +6,21 @@
 //
 
 import Foundation
+import os
+
+extension Logger {
+  static let noArtist = Logger(subsystem: "sql", category: "noArtist")
+}
 
 extension Track {
+  fileprivate var artistName: SortableName {
+    guard let name = (artist ?? albumArtist ?? nil) else {
+      Logger.noArtist.error("\(debugLogInformation, privacy: .public)")
+      return SortableName()
+    }
+    return SortableName(name: name, sorted: (sortArtist ?? sortAlbumArtist) ?? "")
+  }
+
   var rowArtist: RowArtist {
     RowArtist(name: artistName)
   }
--- Sources/iTunes/Track+SQLLogging.swift
+++ Sources/iTunes/Track+SQLLogging.swift
@@ -1,27 +1,14 @@
 //
-//  Track+SQLSafe.swift
+//  Track+SQLLogging.swift
 //
 //
 //  Created by Greg Bolsinga on 1/1/24.
 //
 
 import Foundation
-import os
-
-extension Logger {
-  static let noArtist = Logger(subsystem: "sql", category: "noArtist")
-}
 
 extension Track {
   var debugLogInformation: String {
     "album: \(String(describing: album)), artist: \(String(describing: artist)), kind: \(String(describing: kind)), name: \(name), podcast: \(String(describing: podcast)), trackCount: \(String(describing: trackCount)), trackNumber: \(String(describing: trackNumber)), year: \(String(describing: year))"
-  }
-
-  var artistName: SortableName {
-    guard let name = (artist ?? albumArtist ?? nil) else {
-      Logger.noArtist.error("\(debugLogInformation, privacy: .public)")
-      return SortableName()
-    }
-    return SortableName(name: name, sorted: (sortArtist ?? sortAlbumArtist) ?? "")
   }
 }
