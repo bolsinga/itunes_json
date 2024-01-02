a23176e9a89af923ae6e438301b34c492c958dd8
Greg Bolsinga <bolsinga@mac.com>
2024-01-01 18:38:56 -0800

    Add Track RowArtist extension

---
+++ Sources/iTunes/Track+RowArtist.swift
@@ -0,0 +1,14 @@
+//
+//  Track+RowArtist.swift
+//
+//
+//  Created by Greg Bolsinga on 1/1/24.
+//
+
+import Foundation
+
+extension Track {
+  var rowArtist: RowArtist {
+    RowArtist(self)
+  }
+}
