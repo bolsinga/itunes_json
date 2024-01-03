73bdf1e6244d50b839bc8a968374f95fb49907ab
Greg Bolsinga <bolsinga@mac.com>
2024-01-03 14:32:11 -0800

    QuoteEscaped wraps escaped strings in single quotes like %Q

--- Sources/iTunes/RowKind.swift
+++ Sources/iTunes/RowKind.swift
@@ -11,10 +11,10 @@ struct RowKind: SQLRow {
   @QuoteEscaped var kind: String
 
   var select: String {
-    "SELECT id FROM kinds WHERE name = '\($kind)'"
+    "SELECT id FROM kinds WHERE name = \($kind)"
   }
 
   var insert: String {
-    "INSERT INTO kinds (name) VALUES ('\($kind)');"
+    "INSERT INTO kinds (name) VALUES (\($kind));"
   }
 }
