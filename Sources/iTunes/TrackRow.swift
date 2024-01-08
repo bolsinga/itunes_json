aa46afb61939a3c6bcffb175bb7f3d04b704ed59
Greg Bolsinga <bolsinga@mac.com>
2024-01-08 13:22:23 -0800

    Add TrackRow for TrackRowEncoder

---
+++ Sources/iTunes/TrackRow.swift
@@ -0,0 +1,15 @@
+//
+//  TrackRow.swift
+//
+//
+//  Created by Greg Bolsinga on 1/8/24.
+//
+
+import Foundation
+
+struct TrackRow {
+  typealias SongRow = RowSong<RowArtist, RowAlbum, RowKind>
+
+  let song: SongRow
+  let play: RowPlay<SongRow>?
+}
