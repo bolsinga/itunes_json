151e29a98b9f7688c8d6f3eb1901a77abc84e52f
Greg Bolsinga <bolsinga@mac.com>
2024-01-09 16:11:02 -0800

    Pass artist album kind id in same order always
    - move these ids to the end of the insert statements for convenience for binding in a subsequent diff.

--- Sources/iTunes/RowSong.swift
+++ Sources/iTunes/RowSong.swift
@@ -22,13 +22,13 @@ struct RowSong: TrackRowItem {
 }
 
 extension RowSong {
-  func selectID(kindID: String, albumID: String, artistID: String) -> String {
+  func selectID(artistID: String, albumID: String, kindID: String) -> String {
     "(SELECT id FROM songs WHERE name = \(sql: name.name, options:.safeQuoted) AND itunesid = \(sql: itunesid, options: .quoted) AND artistid = \(sql: artistID) AND albumid = \(sql: albumID) AND kindid = \(sql: kindID) AND tracknumber = \(sql: trackNumber) AND year = \(sql: year) AND size = \(sql: size) AND duration = \(sql: duration) AND dateadded = \(sql: dateAdded, options: .quoted))"
   }
 }
 
 extension RowSong {
-  func insert(kindID: String, albumID: String, artistID: String) -> String {
-    "INSERT INTO songs (name, sortname, itunesid, artistid, albumid, kindid, composer, tracknumber, year, size, duration, dateadded, datereleased, datemodified, comments) VALUES (\(sql: name.name, options:.safeQuoted), \(sql: name.sorted, options:.safeQuoted), \(sql: itunesid, options: .quoted), \(sql: artistID), \(sql: albumID), \(sql: kindID), \(sql: composer, options:.safeQuoted), \(sql: trackNumber), \(sql: year), \(sql: size), \(sql: duration), \(sql: dateAdded, options:.quoted), \(sql: dateReleased, options:.quoted), \(sql: dateModified, options:.quoted), \(sql: comments, options:.safeQuoted));"
+  func insert(artistID: String, albumID: String, kindID: String) -> String {
+    "INSERT INTO songs (name, sortname, itunesid, composer, tracknumber, year, size, duration, dateadded, datereleased, datemodified, comments, artistid, albumid, kindid) VALUES (\(sql: name.name, options:.safeQuoted), \(sql: name.sorted, options:.safeQuoted), \(sql: itunesid, options: .quoted), \(sql: composer, options:.safeQuoted), \(sql: trackNumber), \(sql: year), \(sql: size), \(sql: duration), \(sql: dateAdded, options:.quoted), \(sql: dateReleased, options:.quoted), \(sql: dateModified, options:.quoted), \(sql: comments, options:.safeQuoted), \(sql: artistID), \(sql: albumID), \(sql: kindID));"
   }
 }
