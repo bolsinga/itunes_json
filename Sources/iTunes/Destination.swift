a801a1e76931d9fd26ca75839b6bdeb87eb94046
Greg Bolsinga <bolsinga@mac.com>
2024-01-11 10:18:21 -0800

    improve some source documentation and argument help (#184)

--- Sources/iTunes/Destination.swift
+++ Sources/iTunes/Destination.swift
@@ -7,8 +7,12 @@
 
 import Foundation
 
+/// The destination type for the Track data.
 public enum Destination: CaseIterable {
+  /// Emit a JSON string representing the Tracks.
   case json
+  /// Emit SQLite code that represents the Tracks.
   case sqlCode
+  /// Emit a sqlite3 database that represents the Tracks.
   case db
 }
