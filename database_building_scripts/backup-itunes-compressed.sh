dbe39cb6541bc84590eae0afeb8acb048a9f5aed
Greg Bolsinga <bolsinga@mac.com>
2024-01-16 13:56:40 -0800

    run itunes_json --time-test too and display notification if it fails

--- database_building_scripts/backup-itunes-compressed.sh
+++ database_building_scripts/backup-itunes-compressed.sh
@@ -6,6 +6,12 @@ if [ -z "$DST_DIR" ] ; then
     exit 1
 fi
 
+/Users/bolsinga/Applications/itunes_json/Products/usr/local/bin/itunes_json --time-test
+RESULT=$?
+if [ $RESULT -ne 0 ] ; then
+  /usr/bin/osascript -e "display notification \"Result: $RESULT\" with title \"iTunes Backup Issue\" subtitle \"Time Test\""
+fi
+
 MDATE=`date "+%Y-%m-%d"`
 
 /Users/bolsinga/Applications/itunes_json/Products/usr/local/bin/itunes_json | gzip -c > $DST_DIR/iTunes-$MDATE.json.gz
