07391ad80fd4501246dc964cb2d5bea0d5ba3013
Greg Bolsinga <bolsinga@mac.com>
2024-01-18 14:01:00 -0800

    Add RowSongInterface and Track conforms to it in an extension.

--- Sources/iTunes/Track+RowSong.swift
+++ Sources/iTunes/Track+RowSong.swift
@@ -14,17 +14,29 @@ extension Logger {
   static let noYear = Logger(subsystem: "validation", category: "noYear")
 }
 
-extension Track {
-  fileprivate var dateReleasedISO8601: String {
+extension Track: RowSongInterface {
+  var songPersistentID: UInt {
+    persistentID
+  }
+
+  var songComposer: String {
+    composer ?? ""
+  }
+
+  var songComments: String {
+    comments ?? ""
+  }
+
+  var dateReleasedISO8601: String {
     guard let releaseDate else { return "" }
     return releaseDate.formatted(.iso8601)
   }
 
-  fileprivate var songName: SortableName {
+  var songName: SortableName {
     SortableName(name: name, sorted: sortName ?? "")
   }
 
-  fileprivate var songTrackNumber: Int {
+  var songTrackNumber: Int {
     guard let trackNumber else {
       Logger.noTrackNumber.error("\(debugLogInformation, privacy: .public)")
       return -1
@@ -36,7 +48,7 @@ extension Track {
     return trackNumber
   }
 
-  fileprivate var songYear: Int {
+  var songYear: Int {
     guard let year else {
       Logger.noYear.error("\(debugLogInformation, privacy: .public)")
       return -1
@@ -44,19 +56,12 @@ extension Track {
     return year
   }
 
-  fileprivate var songDuration: Int {
+  var songDuration: Int {
     totalTime ?? -1
   }
 
-  fileprivate var dateAddedISO8601: String {
+  var dateAddedISO8601: String {
     guard let dateAdded else { preconditionFailure() }
     return dateAdded.formatted(.iso8601)
-  }
-
-  var rowSong: RowSong {
-    RowSong(
-      name: songName, itunesid: persistentID, composer: composer ?? "",
-      trackNumber: songTrackNumber, year: songYear, duration: songDuration,
-      dateAdded: dateAddedISO8601, dateReleased: dateReleasedISO8601, comments: comments ?? "")
   }
 }
