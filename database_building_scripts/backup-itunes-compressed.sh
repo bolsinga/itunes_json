a0be524c5249e4b17fc0ec39901dab126b433b7a
Greg Bolsinga <bolsinga@mac.com>
2024-01-21 15:32:13 -0800

    nightly backup of itunes also adds the file to a git repository (#7)
    - The pruned git repository of all the iTunes backups is 28.3 MB
    - The directory of .gz files of the json is 2.16GB!
    - After this works a few times, the .gz directory support will be
    removed.

--- database_building_scripts/backup-itunes-compressed.sh
+++ database_building_scripts/backup-itunes-compressed.sh
@@ -1,10 +1,23 @@
 #/bin/sh
 
+checkFailure() {
+  # $1 exit code
+  # $2 message
+  if [ $1 -ne 0 ] ; then
+    echo "Command failed: $2"
+    exit 1
+  fi
+}
+
 DST_DIR="$1"
 if [ -z "$DST_DIR" ] ; then
     echo "No destination directory"  1>&2
     exit 1
 fi
+
+PRG="$0"
+PRG_HOME=`dirname "$PRG"`
+PRG_HOME=`cd "$PRG_HOME" && pwd`
 
 /Users/bolsinga/Applications/itunes_json/Products/usr/local/bin/itunes_json --time-test
 RESULT=$?
@@ -14,4 +27,21 @@ fi
 
 MDATE=`date "+%Y-%m-%d"`
 
-/Users/bolsinga/Applications/itunes_json/Products/usr/local/bin/itunes_json | gzip -c > $DST_DIR/iTunes-$MDATE.json.gz
+DST_FILE=$DST_DIR/iTunes-$MDATE.json.gz
+
+/Users/bolsinga/Applications/itunes_json/Products/usr/local/bin/itunes_json | gzip -c > $DST_FILE
+
+GIT_DIR=$DST_DIR/../itunes_backups_git/
+
+pushd $GIT_DIR
+git status
+checkFailure $? "$GIT_DIR is not a git directory."
+
+gzip -cd $DST_FILE > itunes.json
+checkFailure $? gzip
+
+$PRG_HOME/database-add-git.sh $GIT_DIR $MDATE
+checkFailure $? database-git
+
+git gc --prune=now
+popd
