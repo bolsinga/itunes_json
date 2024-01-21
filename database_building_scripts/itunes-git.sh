4ba253ee599fe488d0d74cf03657b4b575cf30cd
Greg Bolsinga <bolsinga@mac.com>
2024-01-21 10:07:03 -0800

    add script for wrapping git additions (#1)

--- database_building_scripts/itunes-git.sh
+++ database_building_scripts/itunes-git.sh
@@ -14,6 +14,10 @@ if [ -z "$GIT_DIR" ] ; then
     exit 1
 fi
 
+PRG="$0"
+PRG_HOME=`dirname "$PRG"`
+PRG_HOME=`cd "$PRG_HOME" && pwd`
+
 checkFailure() {
   # $1 exit code
   # $2 message
@@ -31,26 +35,18 @@ SUFFIX=".json.gz"
 
 pushd $GIT_DIR
 git status
-if [ $? -ne 0 ] ; then
-  # Create the empty git repository with:
-  #  git init --initial-branch=main
-  echo "$GIT_DIR is not a git directory."
-  exit 1
-fi
+# Create the empty git repository with:
+#  git init --initial-branch=main
+checkFailure $? "$GIT_DIR is not a git directory."
+popd
 
 for F in $(find $SRC_DIR/ -type f | sort | grep "iTunes-\d\d\d\d-\d\d-\d\d$SUFFIX") ; do
   NAME=`basename -s"$SUFFIX" $F`
   echo "Processing $NAME"
+
   gzip -cd $F > $GIT_DIR/itunes.json
   checkFailure $? gzip
-  git add -A
-  checkFailure $? add
-  git commit -m $NAME
-  if [ $? -ne 0 ] ; then
-    echo "$NAME has no changes."
-  fi
-  git tag $NAME
-  checkFailure $? tag
-done
 
-popd
+  $PRG_HOME/database-add-git.sh $GIT_DIR $NAME
+  checkFailure $? database-git
+done
