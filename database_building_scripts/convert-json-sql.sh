987b49cb57bb1e3ed7c23e02a1700c4d902aa91f
Greg Bolsinga <bolsinga@mac.com>
2024-01-22 09:37:10 -0800

    generate SQL from source git repository (#13)

--- database_building_scripts/convert-json-sql.sh
+++ database_building_scripts/convert-json-sql.sh
@@ -2,31 +2,31 @@
 
 trap "echo Exited!; exit;" SIGINT SIGTERM
 
-JSON_DIR="$1"
-if [ -z "$JSON_DIR" ] ; then
-    echo "No source json directory" 1>&2
+BKUP_DIR="$1"
+if [ -z "$BKUP_DIR" ] ; then
+    echo "No backup directory" 1>&2
     exit 1
 fi
 
-SQL_DIR="$2"
-if [ -z "$SQL_DIR" ] ; then
-    echo "No destination sql directory" 1>&2
+DST_DIR="$2"
+if [ -z "$DST_DIR" ] ; then
+    echo "No destination directory" 1>&2
     exit 1
 fi
 
 JSON_TOOL=~/Applications/itunes_json/Products/usr/local/bin/itunes_json
 
+mkdir -p $DST_DIR
+
 REPAIR=`cat ~/Documents/code/git/web_data/itunes-repair.json`
 
-SUFFIX=".json.gz"
-
-mkdir -p $SQL_DIR
+pushd $BKUP_DIR
 
 COUNT=0
-for F in $(find $JSON_DIR/ -type f | sort | grep "iTunes-\d\d\d\d-\d\d-\d\d$SUFFIX") ; do
-  NAME=`basename -s"$SUFFIX" $F`
+for NAME in $(git tag --list | grep -v empty | sort) ; do
   echo "Processing $NAME"
-  gzip -cd $F | $JSON_TOOL --repair-source "$REPAIR" --json-string --sql-code - | gzip -c > $SQL_DIR/$NAME.sql.gz &
+  git checkout $NAME
+  cat itunes.json | $JSON_TOOL --repair-source "$REPAIR" --json-string --sql-code - | gzip -c > $DST_DIR/$NAME.sql.gz &
   let COUNT++
   if [ $COUNT -eq 7 ]; then
     echo Waiting for Batch
@@ -34,3 +34,7 @@ for F in $(find $JSON_DIR/ -type f | sort | grep "iTunes-\d\d\d\d-\d\d-\d\d$SUFF
     let COUNT=0
   fi
 done
+
+git checkout main
+
+popd
