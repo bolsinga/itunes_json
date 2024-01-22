e6bee1f83a50480614a26b9c03a1fd839ee4b55d
Greg Bolsinga <bolsinga@mac.com>
2024-01-22 12:49:28 -0800

    re-export now reads from git repo (#15)

--- database_building_scripts/re-export-json.sh
+++ database_building_scripts/re-export-json.sh
@@ -2,9 +2,9 @@
 
 trap "echo Exited!; exit;" SIGINT SIGTERM
 
-SRC_DIR="$1"
-if [ -z "$SRC_DIR" ] ; then
-    echo "No source directory" 1>&2
+BKUP_DIR="$1"
+if [ -z "$BKUP_DIR" ] ; then
+    echo "No backup directory" 1>&2
     exit 1
 fi
 
@@ -20,11 +20,12 @@ SUFFIX=".json.gz"
 
 mkdir -p $DST_DIR
 
+pushd $BKUP_DIR
+
 COUNT=0
-for F in $(find $SRC_DIR/ -type f | sort | grep "iTunes-\d\d\d\d-\d\d-\d\d$SUFFIX") ; do
-  NAME=`basename -s"$SUFFIX" $F`
+for NAME in $(git tag --list | grep -v empty | sort) ; do
   echo "Processing $NAME"
-  gzip -cd $F | $JSON_TOOL --json-string --json - | gzip -c > $DST_DIR/$NAME$SUFFIX &
+  cat itunes.json | $JSON_TOOL --json-string --json - | gzip -c > $DST_DIR/$NAME$SUFFIX &
   let COUNT++
   if [ $COUNT -eq 7 ]; then
     echo Waiting for Batch
@@ -32,3 +33,7 @@ for F in $(find $SRC_DIR/ -type f | sort | grep "iTunes-\d\d\d\d-\d\d-\d\d$SUFFI
     let COUNT=0
   fi
 done
+
+git checkout main
+
+popd
