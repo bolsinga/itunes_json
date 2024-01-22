acaccb823d76acfe2b7b15151756df7a1b7ec8b0
Greg Bolsinga <bolsinga@mac.com>
2024-01-21 16:36:36 -0800

    repair reads from the source git directory tags (#10)

--- database_building_scripts/repair-json.sh
+++ database_building_scripts/repair-json.sh
@@ -22,11 +22,13 @@ mkdir -p $REPAIR_DIR
 
 REPAIR=`cat ~/Documents/code/git/web_data/itunes-repair.json`
 
+pushd $BKUP_DIR
+
 COUNT=0
-for F in $(find $BKUP_DIR/ -type f | sort | grep "iTunes-\d\d\d\d-\d\d-\d\d$SUFFIX") ; do
-  NAME=`basename -s"$SUFFIX" $F`
+for NAME in $(git tag --list | grep -v empty | sort) ; do
   echo "Processing $NAME"
-  gzip -cd $F | $JSON_TOOL --repair-source "$REPAIR" --json-string --json - | gzip -c > $REPAIR_DIR/$NAME$SUFFIX &
+  git checkout $NAME
+  cat itunes.json | $JSON_TOOL --repair-source "$REPAIR" --json-string --json - | gzip -c > $REPAIR_DIR/$NAME$SUFFIX &
   let COUNT++
   if [ $COUNT -eq 7 ]; then
     echo Waiting for Batch
@@ -34,3 +36,5 @@ for F in $(find $BKUP_DIR/ -type f | sort | grep "iTunes-\d\d\d\d-\d\d-\d\d$SUFF
     let COUNT=0
   fi
 done
+
+popd
