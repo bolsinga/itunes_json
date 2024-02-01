73d3b9161168b00ae01a2d299ec7cc0f287eac45
Greg Bolsinga <bolsinga@mac.com>
2024-02-01 14:06:20 -0800

    Fix.ignore is now exclusive

--- Sources/iTunes/Repair.swift
+++ Sources/iTunes/Repair.swift
@@ -34,8 +34,21 @@ struct Item: Codable {
 }
 
 extension Fix {
+  /// Tests to see if ignore Fix is exclusive and has no other fixes to apply.
+  internal var validateIgnoreFix: Bool {
+    guard album == nil, artist == nil, playCount == nil, playDate == nil, sortArtist == nil,
+      trackCount == nil, trackNumber == nil, year == nil
+    else { return false }
+    return true
+  }
+
   var trackIgnored: Bool {
     guard let ignore else { return false }
+
+    guard validateIgnoreFix else {
+      preconditionFailure("Fix for kind has other properties set. \(self)")
+    }
+
     return ignore
   }
 }
