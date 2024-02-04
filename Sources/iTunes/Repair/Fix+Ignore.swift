02b5dbe08a3bf55a6dbdb56e0a656342718bba46
Greg Bolsinga <bolsinga@mac.com>
2024-02-03 18:53:09 -0800

    rename some files (#242)

--- Sources/iTunes/Repair/Fix+Ignore.swift
+++ Sources/iTunes/Repair/Fix+Ignore.swift
@@ -1,37 +1,11 @@
 //
-//  Repair+Item.swift
+//  Fix+Ignore.swift
 //
 //
-//  Created by Greg Bolsinga on 2/1/24.
+//  Created by Greg Bolsinga on 2/3/24.
 //
 
 import Foundation
-
-struct Problem: Codable, Hashable {
-  let artist: String?
-  let album: String?
-  let name: String?
-  let playCount: Int?
-  let playDate: Date?
-}
-
-struct Fix: Codable {
-  let album: String?
-  let artist: String?
-  let kind: String?
-  let playCount: Int?
-  let playDate: Date?
-  let sortArtist: String?
-  let trackCount: Int?
-  let trackNumber: Int?
-  let year: Int?
-  let ignore: Bool?
-}
-
-struct Item: Codable {
-  let problem: Problem
-  let fix: Fix
-}
 
 extension Fix {
   /// Tests to see if ignore Fix is exclusive and has no other fixes to apply.
---
+++ Sources/iTunes/Repair/Repair+Load.swift
@@ -0,0 +1,50 @@
+//
+//  Repair+Load.swift
+//
+//
+//  Created by Greg Bolsinga on 2/3/24.
+//
+
+import Foundation
+
+extension Repair {
+  fileprivate enum RepairError: Error {
+    case invalidInput
+    case invalidString
+  }
+
+  public static func create(url: URL?, source: String?) async throws -> Repair {
+    var items: [Item]?
+    if let url { items = try await Repair.load(url: url) }
+    if let source { items = try Repair.load(source: source) }
+    if let items { return Repair(items: items) }
+    throw RepairError.invalidInput
+  }
+
+  private static func load(source: String) throws -> [Item] {
+    guard let data = source.data(using: .utf8) else { throw RepairError.invalidString }
+    return try load(data: data)
+  }
+
+  private static func load(url: URL) async throws -> [Item] {
+    let (data, _) = try await URLSession.shared.data(from: url)
+    return try load(data: data)
+  }
+
+  private static func printRepairJson(items: [Item]) throws {
+    let encoder = JSONEncoder()
+    encoder.outputFormatting = [.sortedKeys, .prettyPrinted]
+    encoder.dateEncodingStrategy = .iso8601
+    let data = try encoder.encode(items)
+    if let string = String(data: data, encoding: .utf8) {
+      print(string)
+    }
+  }
+
+  private static func load(data: Data) throws -> [Item] {
+    let decoder = JSONDecoder()
+    decoder.dateDecodingStrategy = .iso8601
+    let items = try decoder.decode([Item].self, from: data)
+    return items
+  }
+}
