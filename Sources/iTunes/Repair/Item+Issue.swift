086758d71c0f2deb718c6c461bf12b11ae1b8ff7
Greg Bolsinga <bolsinga@mac.com>
2024-02-08 10:21:08 -0800

    Separate [Criterion] validity checks into its own file. (#258)

--- Sources/iTunes/Repair/Item+Issue.swift
+++ Sources/iTunes/Repair/Item+Issue.swift
@@ -7,67 +7,6 @@
 
 import Foundation
 
-extension Array where Element == Criterion {
-  fileprivate struct Qualifier: OptionSet {
-    let rawValue: Int
-
-    static let album = Qualifier(rawValue: 1 << 0)
-    static let artist = Qualifier(rawValue: 1 << 1)
-    static let song = Qualifier(rawValue: 1 << 2)
-
-    static let albumArtistSong: Qualifier = [.album, .artist, .song]
-    static let artistSong: Qualifier = [.artist, .song]
-    static let albumArtist: Qualifier = [.album, .artist]
-  }
-
-  fileprivate var qualifiers: Qualifier {
-    self.reduce(into: Qualifier()) { partialResult, critera in
-      switch critera {
-      case .album(_):
-        partialResult.insert(.album)
-      case .artist(_):
-        partialResult.insert(.artist)
-      case .song(_):
-        partialResult.insert(.song)
-      }
-    }
-  }
-
-  var validForIgnore: Bool {
-    let qualifiers = qualifiers
-    return qualifiers == .artist || qualifiers == .song
-  }
-
-  var validForSortArtist: Bool {
-    qualifiers == .artist
-  }
-
-  var validForKind: Bool {
-    qualifiers == .albumArtistSong
-  }
-
-  var validForYear: Bool {
-    let qualifiers = qualifiers
-    return qualifiers.contains(.album)
-      || qualifiers.intersection(Qualifier.artistSong) == Qualifier.artistSong
-  }
-
-  var validForTrackCount: Bool {
-    // album or artist
-    !qualifiers.intersection(Qualifier.albumArtist).isEmpty
-  }
-
-  var validForAlbum: Bool {
-    // artist or song
-    !qualifiers.intersection(Qualifier.artistSong).isEmpty
-  }
-
-  var validForArtist: Bool {
-    // artist or song
-    !qualifiers.intersection(Qualifier.artistSong).isEmpty
-  }
-}
-
 extension Item {
   var issue: Issue? {
     let issue = Issue(critera: problem.criteria, remedies: fix.remedies)
