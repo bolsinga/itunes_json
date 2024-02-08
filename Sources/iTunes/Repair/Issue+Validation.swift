bf12bfe7c776aa8eb660103493b4c6de7d3f2bd6
Greg Bolsinga <bolsinga@mac.com>
2024-02-08 13:23:23 -0800

    Separate Remedy validation into its own file (#259)

--- Sources/iTunes/Repair/Issue+Validation.swift
+++ Sources/iTunes/Repair/Issue+Validation.swift
@@ -11,13 +11,6 @@ import os
 extension Logger {
   static let noRemedies = Logger(subsystem: "repair", category: "noRemedies")
   static let noCriteria = Logger(subsystem: "repair", category: "noCriteria")
-  static let ignore = Logger(subsystem: "repair", category: "ignore")
-  static let sortArtist = Logger(subsystem: "repair", category: "sortArtist")
-  static let kind = Logger(subsystem: "repair", category: "kind")
-  static let year = Logger(subsystem: "repair", category: "year")
-  static let trackCount = Logger(subsystem: "repair", category: "trackCount")
-  static let album = Logger(subsystem: "repair", category: "album")
-  static let artist = Logger(subsystem: "repair", category: "artist")
 }
 
 extension Issue {
@@ -33,42 +26,8 @@ extension Issue {
     }
 
     for remedy in remedies {
-      switch remedy {
-      case .ignore:
-        guard critera.validForIgnore else {
-          Logger.ignore.error("\(String(describing: self), privacy: .public)")
-          return false
-        }
-      case .repairEmptySortArtist(_):
-        guard critera.validForSortArtist else {
-          Logger.sortArtist.error("\(String(describing: self), privacy: .public)")
-          return false
-        }
-      case .repairEmptyKind(_):
-        guard critera.validForKind else {
-          Logger.kind.error("\(String(describing: self), privacy: .public)")
-          return false
-        }
-      case .repairEmptyYear(_):
-        guard critera.validForYear else {
-          Logger.year.error("\(String(describing: self), privacy: .public)")
-          return false
-        }
-      case .repairEmptyTrackCount(_):
-        guard critera.validForTrackCount else {
-          Logger.trackCount.error("\(String(describing: self), privacy: .public)")
-          return false
-        }
-      case .repairEmptyAlbum(_):
-        guard critera.validForAlbum else {
-          Logger.album.error("\(String(describing: self), privacy: .public)")
-          return false
-        }
-      case .replaceArtist(_):
-        guard critera.validForArtist else {
-          Logger.artist.error("\(String(describing: self), privacy: .public)")
-          return false
-        }
+      if !remedy.validate(critera) {
+        return false
       }
     }
 
