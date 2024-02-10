ba8325dda3572a75be0b33b49a08141602d21f71
Greg Bolsinga <bolsinga@mac.com>
2024-02-10 09:40:41 -0800

    add tests for applying Remedy to Track

---
+++ Tests/iTunes/TrackApplyRemedyTests.swift
@@ -0,0 +1,61 @@
+//
+//  TrackApplyRemedyTests.swift
+//
+//
+//  Created by Greg Bolsinga on 2/10/24.
+//
+
+import XCTest
+
+@testable import iTunes
+
+final class TrackApplyRemedyTests: XCTestCase {
+  func testIgnore() throws {
+    let t = Track(name: "s", persistentID: 0)
+    let r = t.applyRemedy(.ignore)
+
+    XCTAssertNil(r)
+  }
+
+  func testRepairEmptyAlbum() throws {
+    let t = Track(name: "s", persistentID: 0)
+    let r = try XCTUnwrap(t.applyRemedy(.repairEmptyAlbum("l")))
+    let f = try XCTUnwrap(r.album)
+    XCTAssertEqual(f, "l")
+  }
+
+  func testRepairEmptyKind() throws {
+    let t = Track(name: "s", persistentID: 0)
+    let r = try XCTUnwrap(t.applyRemedy(.repairEmptyKind("k")))
+    let f = try XCTUnwrap(r.kind)
+    XCTAssertEqual(f, "k")
+  }
+
+  func testRepairEmptySortArtist() throws {
+    let t = Track(name: "s", persistentID: 0)
+    let r = try XCTUnwrap(t.applyRemedy(.repairEmptySortArtist("s")))
+    let f = try XCTUnwrap(r.sortArtist)
+    XCTAssertEqual(f, "s")
+  }
+
+  func testRepairEmptyTrackCount() throws {
+    let t = Track(name: "s", persistentID: 0)
+    let r = try XCTUnwrap(t.applyRemedy(.repairEmptyTrackCount(3)))
+    let f = try XCTUnwrap(r.trackCount)
+    XCTAssertEqual(f, 3)
+  }
+
+  func testRepairEmptyYear() throws {
+    let t = Track(name: "s", persistentID: 0)
+    let r = try XCTUnwrap(t.applyRemedy(.repairEmptyYear(1970)))
+    let f = try XCTUnwrap(r.year)
+    XCTAssertEqual(f, 1970)
+  }
+
+  func testReplaceArtist() throws {
+    let t = Track(name: "s", persistentID: 0)
+    let r = try XCTUnwrap(t.applyRemedy(.replaceArtist("a")))
+    let f = try XCTUnwrap(r.artist)
+    XCTAssertEqual(f, "a")
+  }
+}
