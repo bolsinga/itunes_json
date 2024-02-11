2cb307a7e378b3859991501f7dc82e9e764f6590
Greg Bolsinga <bolsinga@mac.com>
2024-02-10 20:55:00 -0800

    Add Criterion.playCount

--- Tests/iTunes/CriterionTrackApplicableTests.swift
+++ Tests/iTunes/CriterionTrackApplicableTests.swift
@@ -22,8 +22,36 @@ final class CriterionTrackApplicableTests: XCTestCase {
     [.artist("a"), .song("s")]
   }
 
-  private var allCriterion: Set<Criterion> {
+  private var albumArtistSongCriterion: Set<Criterion> {
     [.album("l"), .artist("a"), .song("s")]
+  }
+
+  private var albumArtistPlayCountCriterion: Set<Criterion> {
+    [.album("l"), .artist("a"), .playCount(3)]
+  }
+
+  private var albumSongPlayCountCriterion: Set<Criterion> {
+    [.album("l"), .song("s"), .playCount(3)]
+  }
+
+  private var artistSongPlayCountCriterion: Set<Criterion> {
+    [.artist("a"), .song("s"), .playCount(3)]
+  }
+
+  private var albumPlayCountCriterion: Set<Criterion> {
+    [.album("l"), .playCount(3)]
+  }
+
+  private var artistPlayCountCriterion: Set<Criterion> {
+    [.artist("a"), .playCount(3)]
+  }
+
+  private var songPlayCountCriterion: Set<Criterion> {
+    [.song("s"), .playCount(3)]
+  }
+
+  private var allCriterion: Set<Criterion> {
+    [.album("l"), .artist("a"), .song("s"), .playCount(3)]
   }
 
   func testSong() throws {
@@ -32,9 +60,17 @@ final class CriterionTrackApplicableTests: XCTestCase {
     XCTAssertFalse(t.criteriaApplies([Criterion.album("l")]))
     XCTAssertFalse(t.criteriaApplies([Criterion.artist("a")]))
     XCTAssertTrue(t.criteriaApplies([Criterion.song("s")]))
+    XCTAssertFalse(t.criteriaApplies([Criterion.playCount(3)]))
     XCTAssertFalse(t.criteriaApplies(albumArtistCriterion))
     XCTAssertFalse(t.criteriaApplies(albumSongCriterion))
     XCTAssertFalse(t.criteriaApplies(artistSongCriterion))
+    XCTAssertFalse(t.criteriaApplies(albumArtistSongCriterion))
+    XCTAssertFalse(t.criteriaApplies(albumArtistPlayCountCriterion))
+    XCTAssertFalse(t.criteriaApplies(albumSongPlayCountCriterion))
+    XCTAssertFalse(t.criteriaApplies(artistSongPlayCountCriterion))
+    XCTAssertFalse(t.criteriaApplies(albumPlayCountCriterion))
+    XCTAssertFalse(t.criteriaApplies(artistPlayCountCriterion))
+    XCTAssertFalse(t.criteriaApplies(songPlayCountCriterion))
     XCTAssertFalse(t.criteriaApplies(allCriterion))
     XCTAssertFalse(t.criteriaApplies([]))
   }
@@ -45,9 +81,17 @@ final class CriterionTrackApplicableTests: XCTestCase {
     XCTAssertTrue(t.criteriaApplies([Criterion.album("l")]))
     XCTAssertFalse(t.criteriaApplies([Criterion.artist("a")]))
     XCTAssertTrue(t.criteriaApplies([Criterion.song("s")]))
+    XCTAssertFalse(t.criteriaApplies([Criterion.playCount(3)]))
     XCTAssertFalse(t.criteriaApplies(albumArtistCriterion))
     XCTAssertTrue(t.criteriaApplies(albumSongCriterion))
     XCTAssertFalse(t.criteriaApplies(artistSongCriterion))
+    XCTAssertFalse(t.criteriaApplies(albumArtistSongCriterion))
+    XCTAssertFalse(t.criteriaApplies(albumArtistPlayCountCriterion))
+    XCTAssertFalse(t.criteriaApplies(albumSongPlayCountCriterion))
+    XCTAssertFalse(t.criteriaApplies(artistSongPlayCountCriterion))
+    XCTAssertFalse(t.criteriaApplies(albumPlayCountCriterion))
+    XCTAssertFalse(t.criteriaApplies(artistPlayCountCriterion))
+    XCTAssertFalse(t.criteriaApplies(songPlayCountCriterion))
     XCTAssertFalse(t.criteriaApplies(allCriterion))
     XCTAssertFalse(t.criteriaApplies([]))
   }
@@ -58,9 +102,44 @@ final class CriterionTrackApplicableTests: XCTestCase {
     XCTAssertTrue(t.criteriaApplies([Criterion.album("l")]))
     XCTAssertTrue(t.criteriaApplies([Criterion.artist("a")]))
     XCTAssertTrue(t.criteriaApplies([Criterion.song("s")]))
+    XCTAssertFalse(t.criteriaApplies([Criterion.playCount(3)]))
     XCTAssertTrue(t.criteriaApplies(albumArtistCriterion))
     XCTAssertTrue(t.criteriaApplies(albumSongCriterion))
     XCTAssertTrue(t.criteriaApplies(artistSongCriterion))
+    XCTAssertTrue(t.criteriaApplies(albumArtistSongCriterion))
+    XCTAssertFalse(t.criteriaApplies(albumArtistPlayCountCriterion))
+    XCTAssertFalse(t.criteriaApplies(albumSongPlayCountCriterion))
+    XCTAssertFalse(t.criteriaApplies(artistSongPlayCountCriterion))
+    XCTAssertFalse(t.criteriaApplies(albumPlayCountCriterion))
+    XCTAssertFalse(t.criteriaApplies(artistPlayCountCriterion))
+    XCTAssertFalse(t.criteriaApplies(songPlayCountCriterion))
+    XCTAssertFalse(t.criteriaApplies(albumArtistPlayCountCriterion))
+    XCTAssertFalse(t.criteriaApplies(albumSongPlayCountCriterion))
+    XCTAssertFalse(t.criteriaApplies(artistSongPlayCountCriterion))
+    XCTAssertFalse(t.criteriaApplies(albumPlayCountCriterion))
+    XCTAssertFalse(t.criteriaApplies(artistPlayCountCriterion))
+    XCTAssertFalse(t.criteriaApplies(songPlayCountCriterion))
+    XCTAssertFalse(t.criteriaApplies(allCriterion))
+    XCTAssertFalse(t.criteriaApplies([]))
+  }
+
+  func testAlbumArtistPlayCountSong() throws {
+    let t = Track(album: "l", artist: "a", name: "s", persistentID: 0, playCount: 3)
+
+    XCTAssertTrue(t.criteriaApplies([Criterion.album("l")]))
+    XCTAssertTrue(t.criteriaApplies([Criterion.artist("a")]))
+    XCTAssertTrue(t.criteriaApplies([Criterion.song("s")]))
+    XCTAssertTrue(t.criteriaApplies([Criterion.playCount(3)]))
+    XCTAssertTrue(t.criteriaApplies(albumArtistCriterion))
+    XCTAssertTrue(t.criteriaApplies(albumSongCriterion))
+    XCTAssertTrue(t.criteriaApplies(artistSongCriterion))
+    XCTAssertTrue(t.criteriaApplies(albumArtistSongCriterion))
+    XCTAssertTrue(t.criteriaApplies(albumArtistPlayCountCriterion))
+    XCTAssertTrue(t.criteriaApplies(albumSongPlayCountCriterion))
+    XCTAssertTrue(t.criteriaApplies(artistSongPlayCountCriterion))
+    XCTAssertTrue(t.criteriaApplies(albumPlayCountCriterion))
+    XCTAssertTrue(t.criteriaApplies(artistPlayCountCriterion))
+    XCTAssertTrue(t.criteriaApplies(songPlayCountCriterion))
     XCTAssertTrue(t.criteriaApplies(allCriterion))
     XCTAssertFalse(t.criteriaApplies([]))
   }
