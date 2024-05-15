4f214ed056712dd9df9da579d31c045e6578f0ad
Greg Bolsinga <bolsinga@mac.com>
2024-05-14 19:03:14 -0700

    SQLBindableInsert now returns arguments for binding as array (#359)
    Leave the binding up to the caller.

--- Sources/iTunes/RowAlbum+SQLBindableInsert.swift
+++ Sources/iTunes/RowAlbum+SQLBindableInsert.swift
@@ -12,30 +12,16 @@ extension RowAlbum: SQLBindableInsert {
     Self.bound { RowAlbum().insert }
   }
 
-  func bindInsert(statement: Database.Statement, ids: [Int64], errorStringBuilder: () -> String)
-    throws
-  {
+  func argumentsForInsert(using ids: [Int64]) throws -> [Database.Value] {
     guard ids.isEmpty else { throw SQLBindingError.noIDsRequired }
 
-    try statement.bind(
-      count: 6,
-      binder: { index in
-        switch index {
-        case 1:
-          Database.Value.string(name.name)
-        case 2:
-          Database.Value.string(name.sorted)
-        case 3:
-          Database.Value.integer(Int64(trackCount))
-        case 4:
-          Database.Value.integer(Int64(discCount))
-        case 5:
-          Database.Value.integer(Int64(discNumber))
-        case 6:
-          Database.Value.integer(Int64(compilation))
-        default:
-          preconditionFailure()
-        }
-      }, errorStringBuilder: errorStringBuilder)
+    return [
+      Database.Value.string(name.name),
+      Database.Value.string(name.sorted),
+      Database.Value.integer(Int64(trackCount)),
+      Database.Value.integer(Int64(discCount)),
+      Database.Value.integer(Int64(discNumber)),
+      Database.Value.integer(Int64(compilation)),
+    ]
   }
 }
