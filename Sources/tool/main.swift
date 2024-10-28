7636a4c18fca5867e5bc69c64602ee26f8d7e347
Greg Bolsinga <bolsinga@mac.com>
2024-10-28 13:09:43 -0700

    Move reading from stdin logic into Program

--- Sources/tool/main.swift
+++ Sources/tool/main.swift
@@ -5,40 +5,4 @@
 //  Created by Greg Bolsinga on 12/11/23.
 //
 
-import Foundation
-
-func readSTDIN() -> String? {
-  var input: String = ""
-
-  while let line = readLine() {
-    if input.isEmpty {
-      input = line
-    } else {
-      input += "\n" + line
-    }
-  }
-
-  return input
-}
-
-var text: String?
-
-var arguments = CommandLine.arguments
-
-if arguments.last == "-" {
-  arguments.removeLast()
-
-  text = readSTDIN()
-}
-
-arguments.removeFirst()
-if let text = text {
-  arguments.insert(text, at: 0)
-}
-
-let command = Program.parseOrExit(arguments)
-do {
-  try await command.run()
-} catch {
-  Program.exit(withError: error)
-}
+await Program.parseStandardInAndArgumentsOrExit(arguments: CommandLine.arguments)
