c50046b2e9644c5d3e09f5759c6d0e99554b3d2c
Greg Bolsinga <bolsinga@mac.com>
2024-11-19 13:32:50 -0800

    Separate into Track.createFromData() too (#435)

--- Sources/iTunes/Track+String.swift
+++ Sources/iTunes/Track+String.swift
@@ -7,19 +7,23 @@
 
 import Foundation
 
-enum JSONDecodingError: Error {
+private enum JSONDecodingError: Error {
   case stringEncodingError
 }
 
 extension Track {
+  static public func createFromData(_ data: Data) throws -> [Track] {
+    let decoder = JSONDecoder()
+    decoder.dateDecodingStrategy = .iso8601
+    return try decoder.decode([Track].self, from: data)
+  }
+
   static public func createFromString(_ source: String?) throws -> [Track] {
     guard let source else {
       preconditionFailure("Should have been caught during ParsableArguments.validate().")
     }
     guard let data = source.data(using: .utf8) else { throw JSONDecodingError.stringEncodingError }
 
-    let decoder = JSONDecoder()
-    decoder.dateDecodingStrategy = .iso8601
-    return try decoder.decode([Track].self, from: data)
+    return try createFromData(data)
   }
 }
