0623d493d2b70860d879fc7e415939a524f7a381
Greg Bolsinga <bolsinga@mac.com>
2024-11-28 09:05:24 -0800

    gatherRepairable now has a generic return type (#472)

--- Sources/iTunes/Gather/Repairable+Gather.swift
+++ Sources/iTunes/Gather/Repairable+Gather.swift
@@ -133,11 +133,11 @@ private func gatherAllKnown<Name: Hashable & Sendable>(
   }
 }
 
-private func gatherRepairable<Name: Hashable & Similar>(
+private func gatherRepairable<Name: Hashable & Similar, Repairable: Sendable>(
   from gitDirectory: URL, gatherCurrentNames: @Sendable () async throws -> [Name],
   namer: @escaping @Sendable ([Track]) -> [Name],
-  mend: @escaping @Sendable (Name, [Name]) -> RepairableArtist
-) async throws -> [RepairableArtist] {
+  mend: @escaping @Sendable (Name, [Name]) -> Repairable
+) async throws -> [Repairable] {
   async let asyncCurrentNames = try await gatherCurrentNames()
 
   let allKnownNames = try await gatherAllKnown(from: gitDirectory, namer: namer)
