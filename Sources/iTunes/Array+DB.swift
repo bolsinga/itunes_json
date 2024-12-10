d69688f74dec7f1610df2bad49763dc592b22c2e
Greg Bolsinga <bolsinga@mac.com>
2024-12-10 11:28:11 -0800

    Create in-memory sqlite3 database (#519)
    - Write its Data out ourselves.
    - This is another step towards making sqlite3 dbs more like the other
    output...

--- Sources/iTunes/Array+DB.swift
+++ Sources/iTunes/Array+DB.swift
@@ -10,12 +10,14 @@ import Foundation
 extension Array where Element == Track {
   func database(
     storage: DatabaseStorage, loggingToken: String?, schemaConstrainsts: SchemaConstraints
-  ) async throws {
+  ) async throws -> Data {
     let dbEncoder = try TracksDBEncoder(
       storage: storage, rowEncoder: self.rowEncoder(loggingToken), loggingToken: loggingToken)
     do {
       try await dbEncoder.encode(schemaConstrainsts: schemaConstrainsts)
+      let data = try await dbEncoder.data()
       await dbEncoder.close()
+      return data
     } catch {
       await dbEncoder.close()
       throw error
