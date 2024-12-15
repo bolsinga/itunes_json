17a20285df283d022abccbffc4c1c9d497cb3bb7
Greg Bolsinga <bolsinga@mac.com>
2024-12-15 15:41:32 -0800

    Use GitLibrary main branch for now. (#556)

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
   ],
   dependencies: [
     .package(url: "https://github.com/apple/swift-argument-parser", from: "1.5.0"),
-    .package(url: "https://github.com/bolsinga/GitLibrary", from: "1.0.0"),
+    .package(url: "https://github.com/bolsinga/GitLibrary", branch: "main"),
   ],
   targets: [
     .target(
