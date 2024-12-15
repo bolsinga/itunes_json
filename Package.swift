7a7f1b2f5012939bbf84e1ff3d1d3667b7cb3b26
Greg Bolsinga <bolsinga@mac.com>
2024-12-14 21:06:48 -0800

    Update to use GitLibrary package

--- Package.swift
+++ Package.swift
@@ -8,18 +8,20 @@ let package = Package(
     .macOS(.v15)
   ],
   products: [
-    .library(name: "Git", targets: ["Git"]),
     .library(name: "iTunes", targets: ["iTunes"]),
     .executable(name: "tunes", targets: ["tunes"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/apple/swift-argument-parser", from: "1.5.0")
+    .package(url: "https://github.com/apple/swift-argument-parser", from: "1.5.0"),
+    .package(url: "https://github.com/bolsinga/GitLibrary", from: "1.0.0"),
   ],
   targets: [
-    .target(name: "Git"),
     .target(
       name: "iTunes",
-      dependencies: [.product(name: "ArgumentParser", package: "swift-argument-parser"), "Git"]),
+      dependencies: [
+        .product(name: "ArgumentParser", package: "swift-argument-parser"),
+        .product(name: "GitLibrary", package: "GitLibrary"),
+      ]),
     .testTarget(name: "iTunesTests", dependencies: ["iTunes"]),
     .executableTarget(name: "tunes", dependencies: [.byName(name: "iTunes")]),
   ]
--- Sources/iTunes/GitTagData.swift
+++ Sources/iTunes/GitTagData.swift
@@ -6,7 +6,7 @@
 //
 
 import Foundation
-import Git
+import GitLibrary
 import os
 
 extension Logger {
