623313a746a432ffe628e13b65ead4981142ec38
Greg Bolsinga <bolsinga@mac.com>
2024-12-22 16:15:02 -0800

    TunesCommand is now part of the library (#562)
    this way the bundled app can call any of the sub commands in the future.
    
    the go() is to workaround
    https://github.com/apple/swift-argument-parser/issues/688

--- Sources/iTunes/Tunes/TunesCommand.swift
+++ Sources/iTunes/Tunes/TunesCommand.swift
@@ -1,9 +1,7 @@
 import ArgumentParser
 import Foundation
-import iTunes
 
-@main
-public struct Program: AsyncParsableCommand {
+public struct TunesCommand: AsyncParsableCommand {
   public static let configuration = CommandConfiguration(
     commandName: "tunes",
     abstract: "A tool for working with iTunes data.",
@@ -13,4 +11,8 @@ public struct Program: AsyncParsableCommand {
   )
 
   public init() {}  // This is public and empty to help the compiler.
+
+  public static func go(_ arguments: [String]?) async {
+    await Self.main(arguments)
+  }
 }
---
+++ Sources/tunes/main.swift
@@ -0,0 +1,10 @@
+//
+//  main.swift
+//  iTunesJson
+//
+//  Created by Greg Bolsinga on 12/22/24.
+//
+
+import iTunes
+
+await TunesCommand.go(CommandLine.arguments)
