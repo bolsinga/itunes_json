bd2cc0b6e28721b3be34b8e51d6d8f23be86433a
Greg Bolsinga <bolsinga@mac.com>
2025-01-02 14:20:04 -0800

    backup now has an optional tagPrefix
    if it is not set, it will read the most recent tag and use that. This will allow it so that the LaunchAgent plist does not have to be updated each time the git repository version changes!

--- Sources/iTunes/BackupContext.swift
+++ Sources/iTunes/BackupContext.swift
@@ -6,11 +6,20 @@
 //
 
 import Foundation
+import GitLibrary
 
 struct BackupContext: Sendable {
   static let defaultTag = "iTunes"
 
   let branch: String
-  let tagPrefix: String
+  let tagPrefix: String?
   let version: String
+
+  func tag(_ git: Git) async throws -> String {
+    if let tagPrefix { return tagPrefix }
+    guard let currentPrefix = try await git.describeTag()?.tagPrefix() else {
+      return Self.defaultTag
+    }
+    return currentPrefix
+  }
 }
