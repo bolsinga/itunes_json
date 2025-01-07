94ccde140c1a936f64cfe0cb9741263dfc366fdf
Greg Bolsinga <bolsinga@mac.com>
2025-01-07 15:30:37 -0800

    Add ability to get the stamp from a tag
    - This is the encoded Date with an iteration version.

--- Tests/iTunes/TagTests.swift
+++ Tests/iTunes/TagTests.swift
@@ -11,94 +11,96 @@ import Testing
 
 struct TagTests {
   @Test func test_StandardFormat() throws {
-    #expect("ZZZ-2024-10-25".tagPrefix() == "ZZZ")
+    #expect(try #require("ZZZ-2024-10-25".tagPrefixAndStamp()) == ("ZZZ", "2024-10-25"))
   }
 
   @Test func test_VersionFormat() throws {
-    #expect("ZZZ-2024-10-25.01".tagPrefix() == "ZZZ")
+    #expect(try #require("ZZZ-2024-10-25.01".tagPrefixAndStamp()) == ("ZZZ", "2024-10-25.01"))
   }
 
   @Test func test_EmptyFormat() throws {
-    #expect("ZZZ-2024-10-25-empty".tagPrefix() == nil)
+    #expect("ZZZ-2024-10-25-empty".tagPrefixAndStamp() == nil)
   }
 
   @Test func test_VersionEmptyFormat() throws {
-    #expect("ZZZ-2024-10-25.02-empty".tagPrefix() == nil)
+    #expect("ZZZ-2024-10-25.02-empty".tagPrefixAndStamp() == nil)
   }
 
   @Test func test_RandomFormat() throws {
-    #expect("ZZZ-2024".tagPrefix() == nil)
-    #expect("ZZZ.2024".tagPrefix() == nil)
-    #expect("ZZZ-2024-10".tagPrefix() == nil)
-    #expect("ZZZ.2024.10".tagPrefix() == nil)
-    #expect("ZZZ-2024-10.25".tagPrefix() == nil)
-    #expect("ZZZ-2024.10.25".tagPrefix() == nil)
+    #expect("ZZZ-2024".tagPrefixAndStamp() == nil)
+    #expect("ZZZ.2024".tagPrefixAndStamp() == nil)
+    #expect("ZZZ-2024-10".tagPrefixAndStamp() == nil)
+    #expect("ZZZ.2024.10".tagPrefixAndStamp() == nil)
+    #expect("ZZZ-2024-10.25".tagPrefixAndStamp() == nil)
+    #expect("ZZZ-2024.10.25".tagPrefixAndStamp() == nil)
   }
 
   @Test func test_EmbeddedHyphen() throws {
-    #expect("ZZZ-A-2024-10-25".tagPrefix() == "ZZZ-A")
-    #expect("ZZZ-A-2024-10-25.01".tagPrefix() == "ZZZ-A")
-    #expect("ZZZ-A-2024-10-25-empty".tagPrefix() == nil)
+    #expect(try #require("ZZZ-A-2024-10-25".tagPrefixAndStamp()) == ("ZZZ-A", "2024-10-25"))
+    #expect(try #require("ZZZ-A-2024-10-25.01".tagPrefixAndStamp()) == ("ZZZ-A", "2024-10-25.01"))
+    #expect("ZZZ-A-2024-10-25-empty".tagPrefixAndStamp() == nil)
   }
 
   @Test func test_EmbeddedPeriod() throws {
-    #expect("ZZZ.A-2024-10-25".tagPrefix() == "ZZZ.A")
-    #expect("ZZZ.A-2024-10-25.01".tagPrefix() == "ZZZ.A")
-    #expect("ZZZ.A-2024-10-25-empty".tagPrefix() == nil)
+    #expect(try #require("ZZZ.A-2024-10-25".tagPrefixAndStamp()) == ("ZZZ.A", "2024-10-25"))
+    #expect(try #require("ZZZ.A-2024-10-25.01".tagPrefixAndStamp()) == ("ZZZ.A", "2024-10-25.01"))
+    #expect("ZZZ.A-2024-10-25-empty".tagPrefixAndStamp() == nil)
   }
 
   @Test func test_MultipleEmbeddedHyphen() throws {
-    #expect("ZZZ-A-B-2024-10-25".tagPrefix() == "ZZZ-A-B")
-    #expect("ZZZ-A-B-2024-10-25.01".tagPrefix() == "ZZZ-A-B")
-    #expect("ZZZ-A-B-2024-10-25-empty".tagPrefix() == nil)
+    #expect(try #require("ZZZ-A-B-2024-10-25".tagPrefixAndStamp()) == ("ZZZ-A-B", "2024-10-25"))
+    #expect(
+      try #require("ZZZ-A-B-2024-10-25.01".tagPrefixAndStamp()) == ("ZZZ-A-B", "2024-10-25.01"))
+    #expect("ZZZ-A-B-2024-10-25-empty".tagPrefixAndStamp() == nil)
   }
 
   @Test func test_MultipleEmbeddedPeriod() throws {
-    #expect("ZZZ.A.B-2024-10-25".tagPrefix() == "ZZZ.A.B")
-    #expect("ZZZ.A.B-2024-10-25.01".tagPrefix() == "ZZZ.A.B")
-    #expect("ZZZ.A.B-2024-10-25-empty".tagPrefix() == nil)
+    #expect(try #require("ZZZ.A.B-2024-10-25".tagPrefixAndStamp()) == ("ZZZ.A.B", "2024-10-25"))
+    #expect(
+      try #require("ZZZ.A.B-2024-10-25.01".tagPrefixAndStamp()) == ("ZZZ.A.B", "2024-10-25.01"))
+    #expect("ZZZ.A.B-2024-10-25-empty".tagPrefixAndStamp() == nil)
   }
 
   @Test func leadingHyphen() throws {
-    #expect("-ZZZ-2024-10-25".tagPrefix() == "-ZZZ")
-    #expect("-ZZZ-2024-10-25.01".tagPrefix() == "-ZZZ")
-    #expect("-ZZZ-2024-10-25-empty".tagPrefix() == nil)
+    #expect(try #require("-ZZZ-2024-10-25".tagPrefixAndStamp()) == ("-ZZZ", "2024-10-25"))
+    #expect(try #require("-ZZZ-2024-10-25.01".tagPrefixAndStamp()) == ("-ZZZ", "2024-10-25.01"))
+    #expect("-ZZZ-2024-10-25-empty".tagPrefixAndStamp() == nil)
   }
 
   @Test func doubleTrailingHyphen() throws {
-    #expect("ZZZ--2024-10-25".tagPrefix() == "ZZZ-")
-    #expect("ZZZ--2024-10-25.01".tagPrefix() == "ZZZ-")
-    #expect("ZZZ--2024-10-25-empty".tagPrefix() == nil)
+    #expect(try #require("ZZZ--2024-10-25".tagPrefixAndStamp()) == ("ZZZ-", "2024-10-25"))
+    #expect(try #require("ZZZ--2024-10-25.01".tagPrefixAndStamp()) == ("ZZZ-", "2024-10-25.01"))
+    #expect("ZZZ--2024-10-25-empty".tagPrefixAndStamp() == nil)
   }
 
   @Test func tripleTrailingHyphen() throws {
-    #expect("ZZZ---2024-10-25".tagPrefix() == "ZZZ--")
-    #expect("ZZZ---2024-10-25.01".tagPrefix() == "ZZZ--")
-    #expect("ZZZ---2024-10-25-empty".tagPrefix() == nil)
+    #expect(try #require("ZZZ---2024-10-25".tagPrefixAndStamp()) == ("ZZZ--", "2024-10-25"))
+    #expect(try #require("ZZZ---2024-10-25.01".tagPrefixAndStamp()) == ("ZZZ--", "2024-10-25.01"))
+    #expect("ZZZ---2024-10-25-empty".tagPrefixAndStamp() == nil)
   }
 
   @Test func leadingPeriod() throws {
-    #expect(".ZZZ-2024-10-25".tagPrefix() == ".ZZZ")
-    #expect(".ZZZ-2024-10-25.01".tagPrefix() == ".ZZZ")
-    #expect(".ZZZ-2024-10-25-empty".tagPrefix() == nil)
+    #expect(try #require(".ZZZ-2024-10-25".tagPrefixAndStamp()) == (".ZZZ", "2024-10-25"))
+    #expect(try #require(".ZZZ-2024-10-25.01".tagPrefixAndStamp()) == (".ZZZ", "2024-10-25.01"))
+    #expect(".ZZZ-2024-10-25-empty".tagPrefixAndStamp() == nil)
   }
 
   @Test func doubleTrailingPeriod() throws {
-    #expect("ZZZ..-2024-10-25".tagPrefix() == "ZZZ..")
-    #expect("ZZZ..-2024-10-25.01".tagPrefix() == "ZZZ..")
-    #expect("ZZZ..-2024-10-25-empty".tagPrefix() == nil)
+    #expect(try #require("ZZZ..-2024-10-25".tagPrefixAndStamp()) == ("ZZZ..", "2024-10-25"))
+    #expect(try #require("ZZZ..-2024-10-25.01".tagPrefixAndStamp()) == ("ZZZ..", "2024-10-25.01"))
+    #expect("ZZZ..-2024-10-25-empty".tagPrefixAndStamp() == nil)
   }
 
   @Test func weirdPrefixes() throws {
-    #expect(".-2024-10-25".tagPrefix() == ".")
-    #expect("..-2024-10-25".tagPrefix() == "..")
-    #expect("--2024-10-25.01".tagPrefix() == "-")
-    #expect("---2024-10-25.01".tagPrefix() == "--")
+    #expect(try #require(".-2024-10-25".tagPrefixAndStamp()) == (".", "2024-10-25"))
+    #expect(try #require("..-2024-10-25".tagPrefixAndStamp()) == ("..", "2024-10-25"))
+    #expect(try #require("--2024-10-25.01".tagPrefixAndStamp()) == ("-", "2024-10-25.01"))
+    #expect(try #require("---2024-10-25.01".tagPrefixAndStamp()) == ("--", "2024-10-25.01"))
   }
 
   @Test func noPrefix() throws {
-    #expect("-2024-10-25".tagPrefix() == nil)
-    #expect("2024-10-25".tagPrefix() == nil)
+    #expect("-2024-10-25".tagPrefixAndStamp() == nil)
+    #expect("2024-10-25".tagPrefixAndStamp() == nil)
   }
 
   @Test func matchingEmpty() throws {
@@ -146,6 +148,6 @@ struct TagTests {
   }
 
   @Test func bad() throws {
-    #expect("iTunes-2024-05-12-empty.01-empty".tagPrefix() == nil)
+    #expect("iTunes-2024-05-12-empty.01-empty".tagPrefixAndStamp() == nil)
   }
 }
