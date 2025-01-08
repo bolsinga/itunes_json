ef31e51fad811657d1724cc211825b1d688e0940
Greg Bolsinga <bolsinga@mac.com>
2025-01-07 20:42:41 -0800

    Add StructuredTag
    - add a String.structuredTag property that will parse the string with RegexBuilder to get the tag version in a sortable struct.
    - this will allow "heterogeneous" lists of tags to be constructed and sorted to workaround the existing results of the bug fixed by #596.

---
+++ Sources/iTunes/StructuredTag.swift
@@ -0,0 +1,28 @@
+//
+//  StructuredTag.swift
+//  itunes_json
+//
+//  Created by Greg Bolsinga on 1/7/25.
+//
+
+import Foundation
+
+struct StructuredTag: Comparable, CustomStringConvertible {
+  let root: String  // "iTunes" from iTunes-V12-2025-01-01
+  let version: Int  // "12" from iTunes-V12-2025-01-01
+  let stamp: String  // "2025-01-01" from iTunes-V12-2025-01-01
+
+  static func < (lhs: Self, rhs: Self) -> Bool {
+    if lhs.root != rhs.root {
+      return lhs.root < rhs.root
+    }
+    if lhs.version != rhs.version {
+      return lhs.version < rhs.version
+    }
+    return lhs.stamp < rhs.stamp
+  }
+
+  var description: String {
+    "\(root)-V\(version)-\(stamp)"
+  }
+}
