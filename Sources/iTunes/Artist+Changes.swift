381e1069a17edbc6cbff350fab943566112883b1
Greg Bolsinga <bolsinga@mac.com>
2025-01-17 16:54:36 -0800

    Keep Set<> as an internal detail for now.
    The sets are created and then immediately made into Arrays.
    
    This is done so that in future diffs the transform can occur for items that are not Hashable.

--- Sources/iTunes/Artist+Changes.swift
+++ Sources/iTunes/Artist+Changes.swift
@@ -8,11 +8,11 @@
 import Foundation
 
 extension Array where Element == Track {
-  var artistNames: Set<SortableName> {
-    Set(self.filter { $0.isSQLEncodable }.compactMap { $0.artistName })
+  var artistNames: [SortableName] {
+    [SortableName](Set(self.filter { $0.isSQLEncodable }.compactMap { $0.artistName }))
   }
 }
 
-func currentArtists() async throws -> Set<SortableName> {
+func currentArtists() async throws -> [SortableName] {
   try await Source.itunes.gather(reduce: false).artistNames
 }
