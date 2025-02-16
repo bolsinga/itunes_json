e9091a6dcfc4db4a07014621964bda3a12e902a0
Greg Bolsinga <bolsinga@mac.com>
2025-02-16 11:15:35 -0800

    Backups can now be Patched
    If an optional patch-url is given to the program, the URL should contain IdentifierCorrections. These corrections will be applied to the Tracks prior to backing up the data.
    
    This way, errant data in the source iTunes data can be patched prior to backup. This is because iTunes has incorrect data within it that is non-editable (chaning dateAdded values is the first one covered).

---
+++ Sources/iTunes/Patchable+URL.swift
@@ -0,0 +1,64 @@
+//
+//  Patchable+URL.swift
+//  itunes_json
+//
+//  Created by Greg Bolsinga on 2/16/25.
+//
+
+import Foundation
+
+extension Dictionary where Key: Codable & Comparable, Value: Codable & Comparable, Key == Value {
+  static fileprivate func load(from url: URL) throws -> Self {
+    try load(from: try Data(contentsOf: url, options: .mappedIfSafe))
+  }
+
+  static fileprivate func load(from data: Data) throws -> Self {
+    let decoder = JSONDecoder()
+    decoder.dateDecodingStrategy = .iso8601
+    return try decoder.decode(Self.self, from: data)
+  }
+}
+
+extension Array where Element: Codable {
+  static fileprivate func load(from url: URL) throws -> Self {
+    try load(from: try Data(contentsOf: url, options: .mappedIfSafe))
+  }
+
+  static fileprivate func load(from data: Data) throws -> Self {
+    let decoder = JSONDecoder()
+    decoder.dateDecodingStrategy = .iso8601
+    return try decoder.decode(Self.self, from: data)
+  }
+}
+
+extension Patchable {
+  func createPatch(_ fileURL: URL) throws -> Patch {
+    switch self {
+    case .artists:
+      Patch.artists(try ArtistPatchLookup.load(from: fileURL))
+    case .albums:
+      Patch.albums(try AlbumPatchLookup.load(from: fileURL))
+    case .missingTitleAlbums:
+      Patch.missingTitleAlbums(try Array<SongArtistAlbum>.load(from: fileURL))
+    case .missingTrackCounts:
+      Patch.trackCounts(try Array<AlbumTrackCount>.load(from: fileURL))
+    case .trackCorrections, .replaceTrackCounts, .replaceDiscCounts, .replaceDiscNumbers:
+      Patch.trackCorrections(try Array<TrackCorrection>.load(from: fileURL))
+    case .missingTrackNumbers:
+      Patch.trackNumbers(try Array<SongTrackNumber>.load(from: fileURL))
+    case .missingYears:
+      Patch.years(try Array<SongYear>.load(from: fileURL))
+    case .songs:
+      Patch.songs(try Array<SongTitleCorrection>.load(from: fileURL))
+    case .replaceDurations, .replacePersistentIds, .replaceDateAddeds, .replaceComposers,
+      .replaceComments, .replaceDateReleased, .replaceAlbumTitle, .replaceSongTitle, .replaceYear,
+      .replaceTrackNumber, .replaceIdSongTitle, .replaceIdDiscCount, .replaceIdDiscNumber,
+      .replaceArtist:
+      try Self.identifierCorrections(fileURL)
+    }
+  }
+
+  static func identifierCorrections(_ fileURL: URL) throws -> Patch {
+    Patch.identifierCorrections(try Array<IdentifierCorrection>.load(from: fileURL))
+  }
+}
