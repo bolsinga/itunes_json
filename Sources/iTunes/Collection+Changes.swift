68baa971853fbd861798a65b9dbf73a7ca6a45dc
Greg Bolsinga <bolsinga@mac.com>
2025-03-07 13:47:52 -0800

    Multiple Changes may apply
    Don't just take the first one. This helps out when there may be a few different repairs available!

--- Sources/iTunes/Collection+Changes.swift
+++ Sources/iTunes/Collection+Changes.swift
@@ -6,15 +6,14 @@
 //
 
 extension Collection where Element: Sendable {
-  func changes<T: Sendable>(createChange: @escaping @Sendable (Element) -> T?) async -> [T] {
-    await withTaskGroup(of: Optional<T>.self) { group in
+  func changes<T: Sendable>(createChange: @escaping @Sendable (Element) -> [T]) async -> [T] {
+    await withTaskGroup(of: Array<T>.self) { group in
       self.forEach { element in
         group.addTask { createChange(element) }
       }
       var changes: [T] = []
       for await change in group {
-        guard let change = change else { continue }
-        changes.append(change)
+        changes.append(contentsOf: change)
       }
       return changes
     }
