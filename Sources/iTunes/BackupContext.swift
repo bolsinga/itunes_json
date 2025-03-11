603800a98c6b89f6c77a96226c33d46a842a53ce
Greg Bolsinga <bolsinga@mac.com>
2025-03-11 16:13:47 -0700

    BackupCommand always reads the latest tags from git (#779)
    This technically "breaks" the very first commit in git. However if
    anyone else should use this in the future, they won't have a git
    repository; they will back up originally to a sqllite db.

--- Sources/iTunes/BackupContext.swift
+++ Sources/iTunes/BackupContext.swift
@@ -10,11 +10,9 @@ import Foundation
 struct BackupContext: Sendable {
   static let defaultTag = "iTunes"
 
-  let tagPrefix: String?
   let version: String
 
   func tag(_ tagProvider: @autoclosure () async throws -> String?) async throws -> String {
-    if let tagPrefix { return tagPrefix }
     guard let currentPrefix = try await tagProvider()?.tagPrefix else {
       return Self.defaultTag
     }
