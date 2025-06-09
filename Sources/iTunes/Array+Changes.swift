64731a92a9b4f9b57fc5c5a8f80b7e97528d381f
Greg Bolsinga <bolsinga@mac.com>
2025-06-09 14:14:05 -0700

    Use Array instead of Collection to remove concurrency warning
    Fixes: `Capture of non-sendable type 'Self.Type' in an isolated closure`

--- Sources/iTunes/Array+Changes.swift
+++ Sources/iTunes/Array+Changes.swift
@@ -1,11 +1,11 @@
 //
-//  Collection+Changes.swift
+//  Array+Changes.swift
 //  itunes_json
 //
 //  Created by Greg Bolsinga on 11/30/24.
 //
 
-extension Collection where Element: Sendable {
+extension Array where Element: Sendable {
   func changes<T: Sendable>(createChange: @escaping @Sendable (Element) -> [T]) async -> [T] {
     await withTaskGroup(of: Array<T>.self) { group in
       self.forEach { element in
