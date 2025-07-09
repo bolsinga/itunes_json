bolsinga/itunes_json

commit 62dd12be4961794dcdac583e1cc4c6e265eda1f5

Store dates in memory as ISO8601 strings instead of Foundation Dates (#851)
- This prevents countless Calenders from being allocated and tossed
while archiving all the git json data to sql.
- Fixes #850

Sources/iTunes/Track+MediaItem.swift
@@ -39,7 +39,7 @@ import Foundation
       if let contentRating = mediaItem.contentRating {
         self.contentRating = contentRating
       }
-      self.dateAdded = mediaItem.addedDate
+      self.dateAdded = mediaItem.addedDate?.formatted(.iso8601)
       self.dateModified = mediaItem.modifiedDate
       if mediaItem.isUserDisabled {
         self.disabled = mediaItem.isUserDisabled
@@ -79,7 +79,7 @@ import Foundation
       }
       self.persistentID = mediaItem.persistentID.uintValue  // Hex?
       self.playCount = mediaItem.playCount
-      self.playDateUTC = mediaItem.lastPlayedDate
+      self.playDateUTC = mediaItem.lastPlayedDate?.formatted(.iso8601)
       if mediaItem.isDRMProtected {
         self.protected = mediaItem.isDRMProtected
       }
@@ -92,7 +92,7 @@ import Foundation
       if mediaItem.isRatingComputed {
         self.ratingComputed = mediaItem.isRatingComputed
       }
-      self.releaseDate = mediaItem.releaseDate
+      self.releaseDate = mediaItem.releaseDate?.formatted(.iso8601)
       self.sampleRate = mediaItem.sampleRate
       self.size = mediaItem.fileSize
       if mediaItem.skipCount != 0 {

Sources/iTunes/Track.swift
@@ -20,7 +20,7 @@ struct Track: Codable, Hashable, Sendable {
   var compilation: Bool? = nil  // "Compilation"
   var composer: String? = nil  // "Composer"
   var contentRating: String? = nil  // "Content Rating"
-  var dateAdded: Date? = nil  // "Date Added"
+  var dateAdded: String? = nil  // "Date Added"
   var dateModified: Date? = nil  // "Date Modified"
   var disabled: Bool? = nil  // "Disabled"
   var discCount: Int? = nil  // "Disc Count"
@@ -40,13 +40,13 @@ struct Track: Codable, Hashable, Sendable {
   var partOfGaplessAlbum: Bool? = nil  // "Part Of Gapless Album"
   var persistentID: UInt  // "Persistent ID"
   var playCount: Int? = nil  // "Play Count"
-  var playDateUTC: Date? = nil  // "Play Date UTC"
+  var playDateUTC: String? = nil  // "Play Date UTC"
   var podcast: Bool? = nil  // "Podcast"
   var protected: Bool? = nil  // "Protected"
   var purchased: Bool? = nil  // "Purchased"
   var rating: Int? = nil  // "Rating"
   var ratingComputed: Bool? = nil  // "Rating Computed"
-  var releaseDate: Date? = nil  // "Release Date"
+  var releaseDate: String? = nil  // "Release Date"
   var sampleRate: Int? = nil  // "Sample Rate"
   var season: Int? = nil  // "Season"
   var series: String? = nil  // "Series"

Sources/iTunes/Array+Codable.swift
@@ -11,7 +11,6 @@ extension Array where Element: Codable {
   func jsonData() throws -> Data {
     let encoder = JSONEncoder()
     encoder.outputFormatting = [.prettyPrinted, .sortedKeys]
-    encoder.dateEncodingStrategy = .iso8601
     return try encoder.encode(self)
   }
 }

Sources/iTunes/CorrectionsDBRow.swift
@@ -46,26 +46,26 @@ extension IdentityRepair.Correction {
     if case .trackNumber(let v) = self { v } else { nil }
   }
   fileprivate var dateadded: String? {
-    if case .dateAdded(let v) = self { v?.formatted(.iso8601) } else { nil }
+    if case .dateAdded(let v) = self { v } else { nil }
   }
   fileprivate var compilation: Int? { nil }
   fileprivate var composer: String? {
     if case .composer(let v) = self { v } else { nil }
   }
   fileprivate var datereleased: String? {
-    if case .dateReleased(let v) = self { v?.formatted(.iso8601) } else { nil }
+    if case .dateReleased(let v) = self { v } else { nil }
   }
   fileprivate var comments: String? {
     if case .comments(let v) = self { v } else { nil }
   }
   fileprivate var oldplaydate: String? {
-    if case .play(let v, _) = self { v.date?.formatted(.iso8601) } else { nil }
+    if case .play(let v, _) = self { v.date } else { nil }
   }
   fileprivate var oldplaycount: Int? {
     if case .play(let v, _) = self { v.count } else { nil }
   }
   fileprivate var newplaydate: String? {
-    if case .play(_, let v) = self { v.date?.formatted(.iso8601) } else { nil }
+    if case .play(_, let v) = self { v.date } else { nil }
   }
   fileprivate var newplaycount: Int? {
     if case .play(_, let v) = self { v.count } else { nil }

Sources/iTunes/DatabaseRowLookup.swift
@@ -30,8 +30,7 @@ extension DatabaseRowLookup {
     return true
   }
 
-  func date(_ key: String) -> Date? {
-    guard let v = string(key) else { return nil }
-    return ISO8601DateFormatter().date(from: v)
+  func date(_ key: String) -> String? {
+    string(key)
   }
 }

Sources/iTunes/Decodable+Array.swift
@@ -18,7 +18,6 @@ extension Decodable {
 
   static func array(from data: Data) throws -> [Self] {
     let decoder = JSONDecoder()
-    decoder.dateDecodingStrategy = .iso8601
     return try decoder.decode([Self].self, from: data)
   }
 

Sources/iTunes/IdentityRepair+Row.swift
@@ -21,7 +21,7 @@ extension DatabaseRowLookup {
   fileprivate var integerValue: Int? { integer("value") }
   fileprivate var stringValue: String? { string("value") }
 
-  fileprivate var date: Date? { date("date") }
+  fileprivate var date: String? { date("date") }
 
   fileprivate var oldPlay: Play { Play(date: date("olddate"), count: integer("oldcount")) }
   fileprivate var newPlay: Play { Play(date: date("newdate"), count: integer("newcount")) }

Sources/iTunes/IdentityRepair.swift
@@ -11,10 +11,10 @@ struct IdentityRepair: Codable, Comparable, Hashable, Identifiable, Sendable {
   enum Correction: Codable, Comparable, Hashable, Sendable {
     case duration(Int?)
     case persistentID(UInt)
-    case dateAdded(Date?)
+    case dateAdded(String?)
     case composer(String)
     case comments(String)
-    case dateReleased(Date?)
+    case dateReleased(String?)
     case albumTitle(SortableName?)
     case year(Int)
     case trackNumber(Int)
@@ -125,7 +125,6 @@ extension IdentityRepair: CustomStringConvertible {
   var description: String {
     let encoder = JSONEncoder()
     encoder.outputFormatting = [.sortedKeys]
-    encoder.dateEncodingStrategy = .iso8601
     guard let data = try? encoder.encode(self) else { return "" }
     return (try? data.asUTF8String()) ?? ""
   }
@@ -135,7 +134,6 @@ extension IdentityRepair.Correction: CustomStringConvertible {
   var description: String {
     let encoder = JSONEncoder()
     encoder.outputFormatting = [.sortedKeys]
-    encoder.dateEncodingStrategy = .iso8601
     guard let data = try? encoder.encode(self) else { return "" }
     return (try? data.asUTF8String()) ?? ""
   }

commit 5d9d2588256c1719777a0a17668daa9f9bb1062b

Track.dateModified should be a String
- Fixes a bug introduced with #851
- The dateModified field in the json output would be garbage (since it is no longer automaticallly encoding).

Sources/iTunes/Track+MediaItem.swift
@@ -40,7 +40,7 @@ import Foundation
         self.contentRating = contentRating
       }
       self.dateAdded = mediaItem.addedDate?.formatted(.iso8601)
-      self.dateModified = mediaItem.modifiedDate
+      self.dateModified = mediaItem.modifiedDate?.formatted(.iso8601)
       if mediaItem.isUserDisabled {
         self.disabled = mediaItem.isUserDisabled
       }

Sources/iTunes/Track.swift
@@ -21,7 +21,7 @@ struct Track: Codable, Hashable, Sendable {
   var composer: String? = nil  // "Composer"
   var contentRating: String? = nil  // "Content Rating"
   var dateAdded: String? = nil  // "Date Added"
-  var dateModified: Date? = nil  // "Date Modified"
+  var dateModified: String? = nil  // "Date Modified"
   var disabled: Bool? = nil  // "Disabled"
   var discCount: Int? = nil  // "Disc Count"
   var discNumber: Int? = nil  // "Disc Number"