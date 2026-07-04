29ab24d8bdd4da0f5bfe18b4ef5c5310211d6576
Greg Bolsinga <bolsinga@mac.com>
2026-07-04 09:29:18 -0700

    Update Package.swift to target OS 26.0 (#867)
    also update spm dependencies, GitLibrary needs a fixed version, since I
    didn't version it properly with a breaking API change.
    
    Co-authored-by: Greg Bolsinga <bolsinga@Gregs-Virtual-Machine.local>

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:6.0
+// swift-tools-version:6.3
 
 import PackageDescription
 
@@ -6,18 +6,18 @@ let package = Package(
   name: "itunes_json",
   defaultLocalization: "en",
   platforms: [
-    .macOS(.v15),
-    .iOS(.v18),
+    .macOS(.v26),
+    .iOS(.v26),
   ],
   products: [
     .library(name: "iTunes", targets: ["iTunes"]),
     .executable(name: "tunes", targets: ["tunes"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/apple/swift-argument-parser", from: "1.6.1"),
-    .package(url: "https://github.com/bolsinga/GitLibrary", from: "1.1.2"),
-    .package(url: "https://github.com/bolsinga/PackageBuildInfo", exact: "2.0.0"),
-    .package(url: "https://github.com/apple/swift-collections.git", from: "1.2.0"),
+    .package(url: "https://github.com/apple/swift-argument-parser", from: "1.8.2"),
+    .package(url: "https://github.com/bolsinga/GitLibrary", exact: "1.1.2"),
+    .package(url: "https://github.com/bolsinga/PackageBuildInfo", exact: "2.0.1"),
+    .package(url: "https://github.com/apple/swift-collections.git", from: "1.6.0"),
   ],
   targets: [
     .target(
