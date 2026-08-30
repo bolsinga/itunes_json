f8e26473a0ba390fab4e25f78a578bca8a6c2b70
Greg Bolsinga <bolsinga@mac.com>
2026-08-30 09:37:31 -0700

    Revert "Update to GitLibrary Subprocess. (#884)"
    This reverts commit 72e89f7aa2f0b0615c5d7c2b3394d8baee05b80e.

--- Sources/iTunes/Repository.swift
+++ Sources/iTunes/Repository.swift
@@ -10,11 +10,11 @@ import GitLibrary
 
 struct Repository {
   private let directory: URL
-  let git: any Git
+  let git: Git
 
   internal init(directory: URL) {
     self.directory = directory
-    self.git = Implementation.subProcess(directory: directory, suppressStandardErr: true)
+    self.git = Implementation.outOfProcess(directory: directory, suppressStandardErr: true)
       .create()
   }
 
